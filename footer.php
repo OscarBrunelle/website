ed518b3396b8d46314203115cea1be1feb1c5c8b
OscarBrunelle <38023020+OscarBrunelle@users.noreply.github.com>
2018-07-20 16:15:00 +0200

    added links

--- footer.php
+++ footer.php
@@ -1,3 +1,5 @@
 <?php
-echo "<p>Copyright &copy; 1999-" . date("Y") . " W3Schools.com</p>";
+  echo '<a href="https://oscarbrunelle.github.io/website/">Link to this website</a>
+    <a href="https://www.w3schools.com">Visit W3Schools.com!</a>';
+  echo "<p>Copyright &copy; 1999-" . date("Y") . " oscarbrunelle.github.io</p>";
 ?>
