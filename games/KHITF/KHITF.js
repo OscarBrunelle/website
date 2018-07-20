afe1011c38cc8447636f10e5bfb28e41085efef8
OscarBrunelle <38023020+OscarBrunelle@users.noreply.github.com>
2018-07-20 16:35:07 +0200

    functions player attack update

--- games/KHITF/KHITF.js
+++ games/KHITF/KHITF.js
@@ -1,1 +1,26 @@
+var Hero = player();
+var Monster = player();
 
+function player(attack = 1, defense = 1, hp = 10, magic = 2){
+  this.attack = attack;
+  this.defense = defense;
+  this.hp = hp;
+  this.magic = magic;
+  var defend = false;
+}
+
+function attack(){
+  if(!Monster.defend){
+    Monster.hp-=Hero.attack;
+  } else {
+    Monster.hp-=(Hero.attack-Monster.defense);
+  }
+  update();
+}
+
+function update(){
+  document.getElementById("hero_health").innerHtml = Hero.health;
+  document.getElementById("monster_health").innerHtml = Monster.health;
+}
+
+update();
