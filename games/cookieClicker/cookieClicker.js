3c4cb16bc9b9593524bb24840915e6115a178b26
OscarBrunelle <38023020+OscarBrunelle@users.noreply.github.com>
2018-07-23 16:49:28 +0200

    updated building price

--- games/cookieClicker/cookieClicker.js
+++ games/cookieClicker/cookieClicker.js
@@ -15,8 +15,7 @@ function closeNav() {
 var money=0;
 var click_value=1;
 
-function Building(base_price=0,price){
-  this.base_price=base_price;
+function Building(price = 0){
   this.price=price;
   this.free=0;
 }
@@ -40,12 +39,12 @@ function buy(building){
   money-=building.price;
   click_value++;
   //building.price*=1.1;
-  building.price= building.base_price * 1.15^(building.owned - building.free);
+  building.price= building.price * 1.15^(building.owned - building.free);
   document.getElementById("cursor_price").innerHTML = "Price: "+building.price+" $";
 
   update();
 }
 
-var cursor = new Building(10,10);
+var cursor = new Building(10);
 
 /* http://cookieclicker.wikia.com/wiki/Buildings */
