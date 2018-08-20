02a03e5401b96d4585fd2c73c138a5d2ca7f8a68
Oscar Brunelle <oscar.brunelle@gmail.com>
2018-08-20 23:16:14 +0200

    made minesweeper work
    added multiple games folders, and minesweeper is now working

--- games/minesweeper/minesweeper.js
+++ games/minesweeper/minesweeper.js
@@ -1,3 +1,14 @@
+/*
+invert height and width
+use a function to get images
+array for images
+inspire more from the old files
+add getter for button class
+*/
+
+var flagMode = false;
+var flagModeButton = document.getElementById("flagModeButton");
+
 var DEFAULT_WIDTH = 20;
 var DEFAULT_HEIGHT = 12;
 var DEFAULT_MINES = 36;
@@ -6,15 +17,16 @@ var width = 20;
 var height = 12;
 var nbrCovered = 20 * 12;
 var nbrUncovered = 0;
+var minesToFind = 36;
 var time = 0;
 
+var model = [];
+
 function update() {
-  covered = document.getElementById("covered");
-  uncovered = document.getElementById("uncovered");
-  timer = document.getElementById("timer");
-  covered.innerHtml = "Cases covered: " + nbrCovered;
-  uncovered.innerHtml = "Cases uncovered: " + nbrUncovered;
-  timer.innerHtml = "Time: " + time;
+  document.getElementById("covered").innerHTML = "Cases covered: " + nbrCovered;
+  document.getElementById("uncovered").innerHTML = "Cases uncovered: " + nbrUncovered;
+  document.getElementById("mines").innerHTML = "Mines to find: " + minesToFind;
+  document.getElementById("timer").innerHTML = "Time: " + time;
 }
 
 function start() {
@@ -37,7 +49,8 @@ function start() {
     mines = 36;
   }
   nbrCovered = width * height;
-  document.getElementById("mines").innerHtml = "Mines to find: " + mines;
+  minesToFind = mines;
+  document.getElementById("mines").innerHTML = "Mines to find: " + mines;
   createField(width, height);
   createMines(mines);
   update();
@@ -45,6 +58,8 @@ function start() {
 
 function createField(width = 20, height = 12) {
   var field = document.getElementById("field");
+  var mini_width = width * 30;
+  //field.setAttribute("min-width",mini_width + "px");
   for (var i = 0; i < height; i++) {
     p = document.createElement("p");
     d = document.createElement("div");
@@ -58,34 +73,56 @@ function createField(width = 20, height = 12) {
       b.setAttribute("class", "case");
       b.setAttribute("pos_x", j);
       b.setAttribute("pos_y", i);
-      b.setAttribute("covered", true);
+      /*b.setAttribute("covered", true);
       b.setAttribute("mined", false);
       b.setAttribute("neighbooringMines", 0);
+      /*
+    	this.iconNumber = iconNumber;
+    	setBackground(Color.WHITE);
+    	setIcon(getImageIcon());
+    	Border emptyBorder = BorderFactory.createEmptyBorder(0, 0, 0, 0);
+    	setBorder(emptyBorder);
+    	setBorderPainted(false);
+      */
       b.appendChild(im);
       d.appendChild(b);
-      //buttons_array[i][j] = b;
     }
     field.appendChild(d);
   }
 }
 
 function createMines(mines = 36) {
-  var model = [height][width];
-  document.getElementById("text").innerHtml = "yo";
-  var txt = 0;
+  var minesToAdd = mines;
   for (var i = 0; i < height; i++) {
+    var table = [];
     for (var j = 0; j < width; j++) {
-      txt++;
-      document.getElementById("text").innerHtml = txt;
-      model[i][j] = new Button();
+      update();
+      var new_button = new Button();
+      table.push(new_button);
+    }
+    model.push(table);
+  }
+  while (minesToAdd > 0) {
+    var i = Math.floor(Math.random() * height);
+    var j = Math.floor(Math.random() * width);
+    if (!model[i][j].isMined) {
+      model[i][j].setMined();
+      minesToAdd--;
     }
   }
-  while (mines > 0) {
-    var i = Math.floor(Math.random() * width);
-    var j = Math.floor(Math.random() * height);
-    if (!model[i][j].isMined()) {
-      model[i][j].isMined = true;
-      mines--;
+
+
+  for (var i = 0; i < height; i++) {
+    for (var j = 0; j < width; j++) {
+      var total = 0;
+      for (var k = Math.max(i - 1, 0); k <= Math.min(i + 1, height - 1); k++) {
+        for (var l = Math.max(j - 1, 0); l <= Math.min(j + 1, width - 1); l++) {
+          if (model[k][l].isMined) {
+            total++;
+          }
+        }
+      }
+      model[i][j].setNeighbooringMines(total);
     }
   }
 }
@@ -94,15 +131,28 @@ function buttonClicked(event) {
   var clicked = event.target;
   var x = clicked.getAttribute("pos_x");
   var y = clicked.getAttribute("pos_y");
-  alert(x);
-  /*if (x.) {
+  var childs = clicked.childNodes;
+  if (flagMode) {
+    childs[0].setAttribute("src", "icons/minesweeper_flag.png");
+    minesToFind--;
+  } else if (model[y][x].isMined) {
+    /*model[y][x].setClicked();
+    uncoverAll();
+    */
+    alert("Oops! That was a mine :/ /br Try again? Press OK to continue");
+    //
+    childs[0].setAttribute("src", "icons/minesweeper_mineSelected.png");
+  } else {
+    var name = getIconFileName(model[y][x].neighbooringMines);
+    childs[0].setAttribute("src", "icons/" + name);
+    nbrCovered--;
+    nbrUncovered++;
+  }
 
-  }*/
-  nbrUncovered++;
-  nbrCovered--;
+  update();
 }
 
-function getIconFileName() {
+function getIconFileName(iconNumber) {
   switch (iconNumber) {
     case 0:
       return "minesweeper_0.png";
@@ -136,9 +186,26 @@ function getIconFileName() {
   }
 }
 
-function Button(isMined = false, neighbooringMines = 0){
+function Button(isMined = false, neighbooringMines = 0) {
   this.isMined = isMined;
   this.neighbooringMines = neighbooringMines;
+  this.covered = true;
+  Button.prototype.setMined = function() {
+    this.isMined = true;
+  };
+  Button.prototype.setNeighbooringMines = function(neighboors) {
+    this.neighbooringMines = neighboors;
+  };
+}
+
+function toggleFlagMode() {
+  if (flagMode == true) {
+    flagMode = false;
+    flagModeButton.innerHTML = "Flag mode: OFF";
+  } else {
+    flagMode = true;
+    flagModeButton.innerHTML = "Flag mode: ON ";
+  }
 }
 
 document.body.onload = createField();
