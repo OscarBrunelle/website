57fae4f58fa65652f7b01ad000d5b60e25df07ab
Oscar Brunelle <oscar.brunelle@gmail.com>
2018-09-06 18:02:08 -0400

    snake fruits
    fruits now spawn and make the snake grow

--- games/snake/snake.js
+++ games/snake/snake.js
@@ -3,12 +3,12 @@ var speedx = 1;
 var speedy = 0;
 var canvas = document.getElementById('myCanvas');
 var ctx = canvas.getContext('2d');
-ctx.fillStyle = 'rgba(0, 0, 0, 0.2)';
 var x = 64;
 var y = 64;
 
 var head;
 var tail = [];
+var fruit;
 
 function drawPlayground() {
 
@@ -34,6 +34,7 @@ function drawPlayground() {
   });
 
   initializeSnake();
+  initializeFruit();
 }
 
 function Snake(xpos = 0, ypos = 0, isHead = false) {
@@ -45,19 +46,47 @@ function Snake(xpos = 0, ypos = 0, isHead = false) {
     this.ypos += yspeed * 20;
   };
   this.draw = function() {
+    ctx.fillStyle = 'rgba(0, 0, 0, 1)';
     ctx.fillRect(this.xpos, this.ypos, 20, 20);
+  };
+  this.eat = function(fruitX, fruitY) {
+    if ((this.xpos <= fruitX && (this.xpos + 20) >= (fruitX+10)) && (this.ypos <= fruitY && (this.ypos + 20) >= (fruitY+10))) {
+      return true;
+    } else {
+      return false;
+    }
   };
 }
 
 function initializeSnake() {
-  head = new Snake(64, 64, true);
+  head = new Snake(0, 0, true);
   head.draw();
   tail.push(head);
 }
 
-function addSnake(){
-  var newS = new Snake();
+function addSnake(x, y) {
+  var newS = new Snake(x, y);
   tail.push(newS);
+}
+
+function initializeFruit() {
+  fruit = new Fruit();
+  fruit.draw();
+}
+
+function Fruit() {
+  this.x = Math.floor( (Math.random() * 631)/32 ) * 32 -10;
+  this.y = Math.floor( (Math.random() * 471)/32 ) * 32-10;
+  this.draw = function(){
+    ctx.fillStyle = 'rgba(255, 255, 255, 1)';
+    ctx.fillRect(this.x, this.y, 10, 10);
+  };
+  this.newFruit = function(){
+    //this.x = Math.floor(Math.random() * 631);
+    //this.y = Math.floor(Math.random() * 471);
+    this.x = Math.floor( (Math.random() * 631)/32 ) * 32-10;
+    this.y = Math.floor( (Math.random() * 471)/32 ) * 32-10;
+  }
 }
 
 function update() {
@@ -66,14 +95,18 @@ function update() {
     if (tail[i].isHead) {
       tail[i].setNewPos(speedx, speedy);
       tail[i].draw();
+      if (tail[i].eat(fruit.x, fruit.y)) {
+        addSnake(tail[tail.length - 1].xpos, tail[tail.length - 1].ypos);
+        fruit.newFruit();
+      }
     } else {
       tail[i].xpos = tail[i - 1].xpos;
       tail[i].ypos = tail[i - 1].ypos;
       tail[i].draw();
     }
   }
+  fruit.draw();
 }
 
 document.body.onload = drawPlayground();
 window.setInterval(update, 200);
-window.setInterval(addSnake,2000);
