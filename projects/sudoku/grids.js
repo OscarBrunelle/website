796cc22b0f502331af93240fcbb9d15065cc4b23
Oscar Brunelle <oscar.brunelle@gmail.com>
2020-01-05 20:49:00 -0500

    minor improvements in the code, moved the grids

---
+++ projects/sudoku/grids.js
@@ -0,0 +1,33 @@
+const exampleGrid = [
+	[null, 3, null, null, 5, 1, 2, 6, null],
+	[1, 2, null, null, null, null, null, 5, 8],
+	[null, null, null, null, null, null, 7, null, 1],
+	[null, null, 3, null, 1, null, 9, null, 7],
+	[null, null, 5, 8, null, 6, 1, null, null],
+	[8, null, 9, null, 2, null, 5, null, null],
+	[5, null, 2, null, null, null, null, null, 9],
+	[3, 9, null, null, null, null, null, 1, 5],
+	[null, 4, 1, 9, 8, null, null, 3, null]
+];
+const exampleGrid2 = [
+	[null, null, 9, null, null, 4, 2, null, null],
+	[null, null, null, null, 8, null, null, 4, null],
+	[6, null, null, 5, 7, null, null, null, 9],
+	[9, null, 3, null, null, null, null, null, null],
+	[null, 1, null, null, null, null, null, 9, null],
+	[null, null, null, null, null, null, 8, null, 5],
+	[7, null, null, null, 1, 9, null, null, 4],
+	[null, 6, null, null, 5, null, null, null, null],
+	[null, null, 1, 7, null, null, 6, null, null]
+];
+const debutant = [
+	[4, null, 5, null, 7, null, null, null, 3],
+	[null, null, null, 1, 3, null, null, null, 8],
+	[null, 2, null, null, 8, null, null, null, 4],
+	[null, null, 4, null, null, null, 7, null, null],
+	[null, 3, null, 5, null, 8, null, 6, null],
+	[null, 5, 1, null, null, null, 4, null, 2],
+	[3, 4, 7, null, null, 9, 2, null, 1],
+	[null, null, null, null, null, null, null, 9, 6],
+	[null, 6, null, null, 1, 2, 8, null, null]
+];
