a928cbfeb2e3e7615cd3020a08308bd4842a2533
Oscar Brunelle <oscar.brunelle@gmail.com>
2020-05-21 14:06:15 +0200

    changed characteristics

--- projects/plantsVsZombies/plants.js
+++ projects/plantsVsZombies/plants.js
@@ -2,19 +2,19 @@ var entitiesCharacteristics = {
     101 : {
         "name" : "peaShooter",
         "cost" : 100,
-        "hp" : 1,
-        "shotSpeed" : 150
+        "hp" : 300,
+        "shotSpeed" : 90
     },
 
     201 : {
         "name" : "pea",
         "speed" : 1,
-        "damage" : 1
+        "damage" : 20
     },
 
     301 : {
         "name" : "basicZombie",
-        "hp" : 4,
+        "hp" : 200,
         "speed" : 0.2,
         "attack" : 1
     }
