5bb69a69fe325a22f6a0ee42da9190dedab4a7f3
Oscar Brunelle <oscar.brunelle@gmail.com>
2020-05-21 12:46:51 +0200

    added show and hide functions

--- shared.js
+++ shared.js
@@ -17,3 +17,19 @@ function get_cookie(cookie_name) {
 function set_cookie(name, value) {
 	document.cookie = name + "=" + value + ";";
 }
+
+/**
+ * Removes the "hidden" class from the jQuery element, removing the "display: none" styling from it.
+ * @param {JQuery DOM Element} element 
+ */
+function show(element) {
+	element.removeClass("hidden");
+}
+
+/**
+ * Adds the "hidden" class to the jQuery element, adding the "display: none" styling to it.
+ * @param {JQuery DOM Element} element 
+ */
+function hide(element) {
+	element.addClass("hidden");
+}
