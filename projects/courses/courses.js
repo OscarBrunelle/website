15fece906e8b9145ba8d273cbc68bdbe67c2a27c
Oscar Brunelle <oscar.brunelle@gmail.com>
2020-05-23 19:47:49 +0200

    initial commit for courses tool

---
+++ projects/courses/courses.js
@@ -0,0 +1,209 @@
+var courses_json = [
+	{
+		"short_name": "CEG3156",
+		"long_name": "Computer Systems Design",
+		"sessions": [
+			{
+				"type": "LEC",
+				"day": 0,
+				"hour": 0,
+				"duration": 1
+			}, {
+				"type": "LEC",
+				"day": 3,
+				"hour": 1,
+				"duration": 1
+			}, {
+				"type": "LAB",
+				"day": 4,
+				"hour": 0,
+				"duration": 2
+			}, {
+				"type": "TUT",
+				"day": 1,
+				"hour": 0,
+				"duration": 1
+			}
+		]
+	}, {
+		"short_name": "CEG3556",
+		"sessions": [
+			{
+				"type": "LEC",
+				"day": 0,
+				"hour": 5,
+				"duration": 1
+			}, {
+				"type": "LEC",
+				"day": 2,
+				"hour": 4,
+				"duration": 1
+			}, {
+				"type": "LAB",
+				"day": 3,
+				"hour": 7,
+				"duration": 2
+			}, {
+				"type": "TUT",
+				"day": 4,
+				"hour": 1,
+				"duration": 1
+			}
+		]
+	}, {
+		"short_name": "CEG3185",
+		"sessions": [
+			{
+				"type": "LEC",
+				"day": 1,
+				"hour": 3,
+				"duration": 1
+			}, {
+				"type": "LEC",
+				"day": 3,
+				"hour": 3,
+				"duration": 1
+			}, {
+				"type": "LAB CHO",
+				"day": 0,
+				"hour": 0,
+				"duration": 1
+			}, {
+				"type": "TUT CHO",
+				"day": 0,
+				"hour": 0,
+				"duration": 1
+			}
+		]
+	},{
+		"short_name": "CEG3585",
+		"long_name": "Comm données et réseautage",
+		"sessions": [
+			{
+				"type": "LEC",
+				"day": 1,
+				"hour": 4,
+				"duration": 1
+			}, {
+				"type": "LEC",
+				"day": 4,
+				"hour": 3,
+				"duration": 1
+			}, {
+				"type": "LAB",
+				"day": 0,
+				"hour": 1,
+				"duration": 1
+			}, {
+				"type": "TUT",
+				"day": 3,
+				"hour": 3,
+				"duration": 1
+			}
+		]
+	}, {
+		"short_name": "CSI3531",
+		"long_name": "Systèmes d'exploitation",
+		"sessions": [
+			{
+				"type": "LEC",
+				"day": 0,
+				"hour": 2,
+				"duration": 1
+			}, {
+				"type": "LEC",
+				"day": 2,
+				"hour": 2,
+				"duration": 1
+			}, {
+				"type": "LAB",
+				"day": 1,
+				"hour": 2,
+				"duration": 1
+			}, {
+				"type": "TUT",
+				"day": 4,
+				"hour": 2,
+				"duration": 1
+			}
+		]
+	}, {
+		"short_name": "ELG3155",
+		"long_name": "Intro To Control Systems",
+		"sessions": [
+			{
+				"type": "LEC",
+				"day": 1,
+				"hour": 1,
+				"duration": 1
+			}, {
+				"type": "LEC",
+				"day": 3,
+				"hour": 0,
+				"duration": 1
+			}, {
+				"type": "LAB",
+				"day": 0,
+				"hour": 5,
+				"duration": 1
+			}, {
+				"type": "TUT",
+				"day": 3,
+				"hour": 6,
+				"duration": 1
+			}
+		]
+	}, {
+		"short_name": "ELG3555",
+		"sessions": [
+			{
+				"type": "LEC",
+				"day": 2,
+				"hour": 1,
+				"duration": 1
+			}, {
+				"type": "LEC",
+				"day": 4,
+				"hour": 0,
+				"duration": 1
+			}, {
+				"type": "LAB",
+				"day": 3,
+				"hour": 4,
+				"duration": 2
+			}, {
+				"type": "TUT",
+				"day": 1,
+				"hour": 1,
+				"duration": 1
+			}
+		]
+	}, {
+		"short_name": "ANP1506",
+		"long_name": "Anatomie humaine et physiologie II",
+		"sessions": [
+			{
+				"type": "LEC",
+				"day": 2,
+				"hour": 5,
+				"duration": 2
+			}
+		]
+	}, {
+		"short_name": "CRM1300",
+		"long_name": "Introduction to Criminology",
+		"sessions": [
+			{
+				"type": "LEC",
+				"day": 0,
+				"hour": 3,
+				"duration": 2
+			}, {
+				"type": "LEC",
+				"day": 2,
+				"hour": 3,
+				"duration": 2
+			}
+		]
+	}
+];
---
+++ projects/courses/index.js
@@ -0,0 +1,150 @@
+const DAYS = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
+const STARTING_TIME = 8.5;
+const JET_LAG = 6;
+const SHIFT_LENGTH = 1.5;
+
+/*
+add export / import
+choose colors
+add course
+*/
+
+$(function() {
+	create_grid();
+	create_course_checkboxes();
+	update_grid(get_previously_selected_courses());
+});
+
+function create_grid() {
+	$("#week_grid").empty();
+
+	const col_h = $("<div id='column-hours'><div></div></div>").appendTo("#week_grid");
+	for (let h = 0; h < 9; h++) {
+		const time = (STARTING_TIME + JET_LAG + SHIFT_LENGTH * h) % 24;
+		const hours = ("0" + parseInt(time)).substr(-2);
+		const minutes = ("0" + time % 1 * 60).substr(-2);
+		const text = hours + "h" + minutes;
+		col_h.append("<div>" + text + "</div>");
+	}
+	for (let d = 0; d < 5; d++) {
+		const col_d = $("<div id='column-" + d + "'></div>").appendTo("#week_grid");
+		col_d.append("<div>" + DAYS[d] + "</div>");
+		for (let h = 0; h < 9; h++) {
+			col_d.append("<div id='D" + d + "H" + h + "'></div>");
+		}
+	}
+}
+
+function display_classes(selected_courses) {
+	for (const c_index in courses_json) {
+		const course = courses_json[c_index];
+		if (selected_courses.indexOf(course.short_name) > -1) {
+			for (let i = 0; i < course.sessions.length; i++) {
+				let session = course.sessions[i];
+				let parent = $("<div class='" + course.short_name + "'></div>").appendTo("#D" + session.day + "H" + session.hour);
+				parent.addClass("course_color-" + c_index);
+				parent.append("<span class='course_name'>" + course.short_name + " - " + session.type + "</span>");
+				if ($("#option-long_name").is(":checked")) {
+					parent.append("<span class='course_name'>" + course.long_name + "</span>");
+				}
+				if (session.duration === 2) {
+					$("#D" + session.day + "H" + (session.hour + 1)).append(parent.clone());
+				}
+			}
+		}
+	}
+}
+
+function create_course_checkboxes() {
+	$("#course_checkboxes").empty();
+
+	const select_all = $("<input id='checkbox-all' type='checkbox' value='all'></input>").appendTo("#course_checkboxes");
+	$("<label for='checkbox-all'>Select / Deselect all</label><br />").appendTo("#course_checkboxes");
+	select_all.on("change", function() {
+		const checked = $(this).is(":checked");
+		$.each($("input[name='selected_courses']"), function() {
+			$(this).prop("checked", checked).trigger("change");
+		});
+	});
+
+	let all_checked = true;
+	const selected_courses = get_previously_selected_courses();
+	for (const c_index in courses_json) {
+		const course = courses_json[c_index];
+		const checked = (selected_courses.indexOf(course.short_name) > -1) ? " checked" : "";
+		if (!checked) all_checked = false;
+		const input = $("<input id='checkbox-" + course.short_name + "' type='checkbox' name='selected_courses' value='" + course.short_name + "'" + checked + "></input>").appendTo("#course_checkboxes");
+		$("<label for='checkbox-" + course.short_name + "'>" + course.short_name + "</label><br />").appendTo("#course_checkboxes");
+		input.on("change", function() {
+			let all_checked = true;
+			if (!$(this).is(":checked")) {
+				all_checked = false;
+			} else {
+				$.each($("input[name='selected_courses']"), function() {
+					if (!$(this).is(":checked")) {
+						all_checked = false;
+					}
+				});
+			}
+			select_all.prop("checked", all_checked);
+			update_grid();
+		});
+	}
+
+	select_all.prop("checked", all_checked);
+}
+
+function update_grid() {
+	create_grid();
+	const selected_courses = get_selected_courses();
+	display_classes(selected_courses);
+	set_cookie("selected_courses", JSON.stringify(selected_courses));
+	set_session_storage("selected_courses", JSON.stringify(selected_courses));
+}
+
+function get_cookie(cookie_name) {
+	const name = cookie_name + "=";
+	const decodedCookie = decodeURIComponent(document.cookie);
+	const ca = decodedCookie.split(";");
+	for(let i = 0; i <ca.length; i++) {
+		let c = ca[i];
+		while (c.charAt(0) == " ") {
+			c = c.substring(1);
+		}
+		if (c.indexOf(name) == 0) {
+			return c.substring(name.length, c.length);
+		}
+	}
+	return "";
+}
+
+function set_cookie(name, value) {
+	document.cookie = name + "=" + value + ";";
+}
+
+function get_session_storage(name) {
+	return sessionStorage.getItem(name);
+}
+
+function set_session_storage(name, value) {
+	sessionStorage.setItem(name, value);
+}
+
+function get_previously_selected_courses() {
+	let cookie = get_cookie("selected_courses");
+	cookie = get_session_storage("selected_courses");
+	if (cookie != null && cookie !== "") {
+		cookie = JSON.parse(cookie);
+	} else {
+		cookie = [];
+	}
+	return cookie;
+}
+
+function get_selected_courses() {
+	let selected_courses = [];
+	$.each($("input[name='selected_courses']:checked"), function(){
+		selected_courses.push($(this).val());
+	});
+	return selected_courses;
+}
