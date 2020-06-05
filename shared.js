2946a22e83e2a658fa9206acfa24937668f18640
Oscar Brunelle <oscar.brunelle@gmail.com>
2020-06-05 03:05:38 +0200

    added new useful functions

--- shared.js
+++ shared.js
@@ -1,8 +1,34 @@
+const FA_ICONS = "../../icons/svgs/";
+
+class LanguageSelector {
+	constructor(parent_selector, _languages, def_lang_index, _click_func) {
+		const ref = this;
+		this.languages = _languages;
+		this.lang_index = def_lang_index;
+		this.language = this.languages[this.lang_index];
+		this.click_func = _click_func;
+
+		this.element = $("<button id='language_selector'>" + this.language + "</button>").appendTo(parent_selector);
+		this.element.on("click", function () {
+			ref.switch_language()
+		});
+	}
+
+	switch_language() {
+		this.lang_index = (this.lang_index + 1) % this.languages.length;
+		this.language = this.languages[this.lang_index];
+		this.element.text(this.language);
+		if (this.click_func != null) {
+			this.click_func();
+		}
+	}
+}
+
 function get_cookie(cookie_name) {
 	const name = cookie_name + "=";
 	const decodedCookie = decodeURIComponent(document.cookie);
 	const ca = decodedCookie.split(";");
-	for(let i = 0; i <ca.length; i++) {
+	for (let i = 0; i < ca.length; i++) {
 		let c = ca[i];
 		while (c.charAt(0) == " ") {
 			c = c.substring(1);
@@ -32,4 +58,86 @@ function show(element) {
  */
 function hide(element) {
 	element.addClass("hidden");
+}
+
+function print_element(element_id, css_file_path = null) {
+	let html_to_print = "";
+	if (css_file_path != null) {
+		html_to_print += "<link rel='stylesheet' type='text/css' href='resume.css'>";
+	}
+	const DocumentContainer = document.getElementById(element_id);
+	html_to_print += DocumentContainer.outerHTML;
+
+	const WindowObject = window.open("");
+	WindowObject.document.writeln(html_to_print);
+	WindowObject.document.close();
+	WindowObject.focus();
+	WindowObject.print();
+	WindowObject.close();
+}
+
+function read_file(element_selector, finished_function, log_progress = false) {
+	if (document.querySelector(element_selector).files.length <= 0) {
+		alert("Error : No file selected");
+		return;
+	}
+
+	// first file selected by user
+	var file = document.querySelector(element_selector).files[0];
+
+	// perform validation on file type & size if required
+
+	// read the file
+	var reader = new FileReader();
+
+	// file reading started
+	reader.addEventListener("loadstart", function () {
+		if (log_progress) {
+			console.log("File reading started");
+		}
+	});
+
+	// file reading finished successfully
+	reader.addEventListener("load", function (e) {
+		// contents of file in variable     
+		const data = e.target.result;
+		finished_function(data);
+	});
+
+	// file reading failed
+	reader.addEventListener("error", function () {
+		alert("Error : Failed to read file");
+	});
+
+	// file read progress 
+	reader.addEventListener("progress", function (e) {
+		if (e.lengthComputable == true && log_progress) {
+			var percent_read = Math.floor((e.loaded / e.total) * 100);
+			console.log(percent_read + "% read");
+		}
+	});
+
+	// read as text file
+	reader.readAsText(file);
+}
+
+// Function to download data to a file
+function download(data, filename, type) {
+	const file = new Blob([data], {
+		type: type
+	});
+	if (window.navigator.msSaveOrOpenBlob) // IE10+
+		window.navigator.msSaveOrOpenBlob(file, filename);
+	else { // Others
+		const a = document.createElement("a"),
+			url = URL.createObjectURL(file);
+		a.href = url;
+		a.download = filename;
+		document.body.appendChild(a);
+		a.click();
+		setTimeout(function () {
+			document.body.removeChild(a);
+			window.URL.revokeObjectURL(url);
+		}, 0);
+	}
 }
