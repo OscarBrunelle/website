62b3059db29fc390d908151c5327e7bfc2d811d3
Oscar Brunelle <oscar.brunelle@gmail.com>
2020-07-22 00:41:15 +0200

    added toggle obstacle on click

--- projects/astar/astar.js
+++ projects/astar/astar.js
@@ -1,29 +1,43 @@
-const DEFAULT_NUMBER_FRAMES_X = 15;
-let number_frames_x = DEFAULT_NUMBER_FRAMES_X;
-const DEFAULT_NUMBER_FRAMES_Y = 10;
-let number_frames_y = DEFAULT_NUMBER_FRAMES_Y;
+const DEFAULT_NUMBER_NODES_X = 15;
+let number_nodes_x = DEFAULT_NUMBER_NODES_X;
+const DEFAULT_NUMBER_NODES_Y = 10;
+let number_nodes_y = DEFAULT_NUMBER_NODES_Y;
 const DEFAULT_NUMBER_OBSTACLES = 50;
 let number_obstacles = DEFAULT_NUMBER_OBSTACLES;
 
-const grid = new Grid("#grid-div", 600, 400, number_frames_x, number_frames_y);
+let grid;
 
-let frames = [];
-let best_path = [];
+let nodes = [];
 let start = {
 	x: 0,
 	y: 0
 };
 let end = {
-	x: number_frames_x - 1,
-	y: number_frames_y - 1
+	x: number_nodes_x - 1,
+	y: number_nodes_y - 1
 };
 
 function load() {
+	grid = new Grid("#grid-div", 600, 400, number_nodes_x, number_nodes_y, "grid");
+
 	create_grid();
 
+	grid.onclick((x, y) => {
+		let pos = grid.get_index_of_pos(x, y);
+		let node = get_node(pos.x, pos.y);
+		clearInterval(best_path_interval);
+		node.is_obstacle = !node.is_obstacle;
+		find_best_path();
+	});
+	grid.onredraw = function () {
+		for (const node of nodes) {
+			node.draw();
+		}
+	};
+
 	document.getElementById("number_obstacles-input").addEventListener("change", parameter_change);
-	document.getElementById("number_frames_x-input").addEventListener("change", parameter_change);
-	document.getElementById("number_frames_y-input").addEventListener("change", parameter_change);
+	document.getElementById("number_nodes_x-input").addEventListener("change", parameter_change);
+	document.getElementById("number_nodes_y-input").addEventListener("change", parameter_change);
 	document.getElementById("start-x-input").addEventListener("change", parameter_change);
 	document.getElementById("start-y-input").addEventListener("change", parameter_change);
 	document.getElementById("end-x-input").addEventListener("change", parameter_change);
@@ -39,7 +53,7 @@ function parameter_change(event) {
 
 	switch (element.name) {
 		case "number_obstacles":
-			if (value >= 0 && value <= (number_frames_x * number_frames_y - 2)) {
+			if (value >= 0 && value <= (number_nodes_x * number_nodes_y - 2)) {
 				number_obstacles = value;
 			} else {
 				//element.parentElement.querySelector("label[name='" + element.name + "']")
@@ -47,47 +61,47 @@ function parameter_change(event) {
 				return;
 			}
 			break;
-		case "number_frames_x":
-			if (value > 0 && number_obstacles <= (value * number_frames_y - 2)) {
-				number_frames_x = value;
-				grid.number_frames_x = number_frames_x;
+		case "number_nodes_x":
+			if (value > 0 && number_obstacles <= (value * number_nodes_y - 2)) {
+				number_nodes_x = value;
+				grid.number_nodes_x = number_nodes_x;
 				grid.resize();
 			} else {
 				return;
 			}
 			break;
-		case "number_frames_y":
-			if (value > 0 && number_obstacles <= (number_frames_x * value - 2)) {
-				number_frames_y = value;
-				grid.number_frames_y = number_frames_y;
+		case "number_nodes_y":
+			if (value > 0 && number_obstacles <= (number_nodes_x * value - 2)) {
+				number_nodes_y = value;
+				grid.number_nodes_y = number_nodes_y;
 				grid.resize();
 			} else {
 				return;
 			}
 			break;
 		case "start-x":
-			if (value >= 0 && value < number_frames_x) {
+			if (value >= 0 && value < number_nodes_x) {
 				start.x = value;
 			} else {
 				return;
 			}
 			break;
 		case "start-y":
-			if (value >= 0 && value < number_frames_y) {
+			if (value >= 0 && value < number_nodes_y) {
 				start.y = value;
 			} else {
 				return;
 			}
 			break;
 		case "end-x":
-			if (value >= 0 && value < number_frames_x) {
+			if (value >= 0 && value < number_nodes_x) {
 				end.x = value;
 			} else {
 				return;
 			}
 			break;
 		case "end-y":
-			if (value >= 0 && value < number_frames_y) {
+			if (value >= 0 && value < number_nodes_y) {
 				end.y = value;
 			} else {
 				return;
@@ -97,158 +111,81 @@ function parameter_change(event) {
 			return;
 	}
 
-	document.getElementById("number_obstacles-input").max = (number_frames_x * number_frames_y - 2);
-	document.getElementById("start-x-input").max = number_frames_x - 1;
-	document.getElementById("start-y-input").max = number_frames_y - 1;
-	document.getElementById("end-x-input").max = number_frames_x - 1;
-	document.getElementById("end-y-input").max = number_frames_y - 1;
+	document.getElementById("number_obstacles-input").max = (number_nodes_x * number_nodes_y - 2);
+	document.getElementById("start-x-input").max = number_nodes_x - 1;
+	document.getElementById("start-y-input").max = number_nodes_y - 1;
+	document.getElementById("end-x-input").max = number_nodes_x - 1;
+	document.getElementById("end-y-input").max = number_nodes_y - 1;
 
 	create_grid();
 }
 
 function create_grid() {
-	if (number_obstacles > (number_frames_x * number_frames_y - 2)) {
+	if (number_obstacles > (number_nodes_x * number_nodes_y - 2)) {
 		return;
 	} else if (
-		start.x < 0 || start.x >= number_frames_x ||
-		start.y < 0 || start.y >= number_frames_y ||
-		end.x < 0 || end.x >= number_frames_x ||
-		end.y < 0 || end.y >= number_frames_y ||
+		start.x < 0 || start.x >= number_nodes_x ||
+		start.y < 0 || start.y >= number_nodes_y ||
+		end.x < 0 || end.x >= number_nodes_x ||
+		end.y < 0 || end.y >= number_nodes_y ||
 		(start.x === end.x && start.y === end.y)
 	) {
 		return;
 	}
-	frames = [];
+	clearInterval(best_path_interval);
+	nodes = [];
 
-	for (let index_x = 0; index_x < number_frames_x; index_x++) {
-		for (let index_y = 0; index_y < number_frames_y; index_y++) {
-			let frame = new GridFrame(index_x, index_y);
+	for (let index_x = 0; index_x < number_nodes_x; index_x++) {
+		for (let index_y = 0; index_y < number_nodes_y; index_y++) {
+			let node = new GridNode(index_x, index_y);
 			if (index_x === start.x && index_y === start.y) {
-				frame.is_start = true;
+				node.is_start = true;
 			} else if (index_x === end.x && index_y === end.y) {
-				frame.is_end = true;
+				node.is_end = true;
 			}
-			frames.push(frame);
+			nodes.push(node);
 		}
 	}
 
 	for (let i = number_obstacles; i > 0;) {
-		let index_x = Math.floor(Math.random() * number_frames_x);
-		let index_y = Math.floor(Math.random() * number_frames_y);
-		let frame = get_node(index_x, index_y);
-		if (!frame.is_obstacle) {
-			frame.is_obstacle = true;
+		let index_x = Math.floor(Math.random() * number_nodes_x);
+		let index_y = Math.floor(Math.random() * number_nodes_y);
+		let node = get_node(index_x, index_y);
+		if (!node.is_obstacle) {
+			node.is_obstacle = true;
 			i--;
 		}
 	}
 
-	best_path = find_best_path();
-	/*for (const grid_frame of frames) {
-		if (best_path.path.indexOf(grid_frame) >= 0) {
-			grid_frame.is_path = true;
-		}
-	}*/
-
-	draw_frames();
+	draw_nodes();
+	find_best_path();
 }
 
-function draw_frames() {
+function draw_nodes() {
 	grid.clear();
-	for (const grid_frame of frames) {
-		grid_frame.draw();
+	for (const node of nodes) {
+		node.draw();
 	}
 }
 
 function draw_single_path(path) {
 	grid.clear();
-	for (const grid_frame of frames) {
-		if (path.indexOf(grid_frame) >= 0) {
-			grid_frame.color = "blue";
+	for (const node of nodes) {
+		if (path.indexOf(node) >= 0) {
+			node.color = "blue";
 		}
-		grid_frame.draw();
-		grid_frame.update_color();
+		node.draw();
+		node.update_color();
 	}
 }
 
 function get_node(grid_x, grid_y) {
-	for (const grid_frame of frames) {
-		if (grid_frame.grid_x === grid_x && grid_frame.grid_y === grid_y) {
-			return grid_frame;
+	for (const node of nodes) {
+		if (node.grid_x === grid_x && node.grid_y === grid_y) {
+			return node;
 		}
 	}
 	return null;
-}
-
-class GridFrame extends Drawable {
-	constructor(grid_x, grid_y) {
-		super(grid, {
-			grid_x: grid_x,
-			grid_y: grid_y,
-		});
-
-		this.is_obstacle = false;
-		this.is_path = false;
-		this.is_start = false;
-		this.is_end = false;
-	}
-
-	update_color() {
-		if (this.is_start) {
-			this.color = "green";
-		} else if (this.is_end) {
-			this.color = "red";
-		} else if (this.is_obstacle) {
-			this.color = "grey";
-		} else if (this.is_path) {
-			this.color = "blue";
-		} else {
-			this.color = null;
-		}
-	}
-
-	set is_obstacle(value) {
-		if (this.is_start || this.is_end || this.is_obstacle === value) {
-			return false;
-		}
-		this.obstacle = value;
-		this.update_color();
-		return true;
-	}
-
-	get is_obstacle() {
-		return this.obstacle != null ? this.obstacle : false;
-	}
-
-	set is_path(value) {
-		if (this.is_start || this.is_end || this.is_obstacle) {
-			return false;
-		}
-		this.path = value;
-		this.update_color();
-		return true;
-	}
-
-	get is_path() {
-		return this.path != null ? this.path : false;
-	}
-
-	set is_start(value) {
-		this.start = value;
-		this.update_color();
-	}
-
-	get is_start() {
-		return this.start != null ? this.start : false;
-	}
-
-	set is_end(value) {
-		this.end = value;
-		this.update_color();
-	}
-
-	get is_end() {
-		return this.end != null ? this.end : false;
-	}
 }
 
 function parse_input(value, int_value) {
@@ -263,26 +200,19 @@ function parse_input(value, int_value) {
 let nodes_queue = [];
 let nodes_done = [];
 let best_path_interval;
+
 function find_best_path() {
+	clearInterval(best_path_interval);
 	nodes_queue = [];
 	nodes_done = [];
+	for (const node of nodes) {
+		node.is_path = false;
+	}
+	draw_nodes();
 	let starting_node = get_node(start.x, start.y);
 	starting_node.g = 0;
 	nodes_queue.push(starting_node);
-	/*if (path_cost > -1) {
-		for (const node of nodes_done) {
-			console.log(node.f);
-			if (node.f === path_cost) {
-				node.is_path = true;
-				node.draw();
-			} else {
-				node.color = "cyan";
-				node.draw();
-			}
-		}
-	}*/
 
-	clearInterval(best_path_interval);
 	best_path_interval = setInterval(find_best_path_call, 25);
 }
 
@@ -293,19 +223,18 @@ function find_best_path_call() {
 	}
 	let node = nodes_queue.pop();
 	nodes_done.push(node);
-	node.color = "cyan";
-	node.draw();
-	if (node === get_node(end.x, end.y)) {
+	let end_node = get_node(end.x, end.y);
+	if (node !== get_node(start.x, start.y) && node !== end_node) {
+		node.color = "cyan";
+		node.draw();
+	}
+	if (node === end_node) {
 		clearInterval(best_path_interval);
-		let path = retrieve_path([node]);
-		if (path != null) {
-			for (const path_node of path) {
-				path_node.is_path = true;
-			}
-			draw_frames();
-		} else {
-			console.error("Error: Can't retrieve the path.");
+		let path = retrieve_path();
+		for (const path_node of path) {
+			path_node.is_path = true;
 		}
+		draw_nodes();
 		return;
 	}
 	let neighbours = get_neighbours(node);
@@ -359,9 +288,9 @@ function get_neighbours(parent_node) {
 	return neighbours;
 }
 
-function retrieve_path(path) {
-	if (path == null) {
-		return null;
+function retrieve_path(path = [get_node(end.x, end.y)]) {
+	if (path.length < 1) {
+		return [];
 	}
 	let node = path[0];
 	let neighbours = get_neighbours(node);
@@ -371,16 +300,82 @@ function retrieve_path(path) {
 			path.unshift(neighbour);
 			return path;
 		}
-		if (min_f == null || (neighbour.f != null && neighbour.f < min_f)) {
+		if (path.indexOf(neighbour) < 0 && (min_f == null || (neighbour.f != null && neighbour.f < min_f))) {
 			min_f = neighbour.f;
 			min_neighbour = neighbour;
 		}
 	}
 	if (min_f == null || min_neighbour == null) {
-		return null;
+		return [];
 	}
 	path.unshift(min_neighbour);
 	return retrieve_path(path);
+}
+
+class GridNode extends Drawable {
+	constructor(grid_x, grid_y) {
+		super(grid, {
+			grid_x: grid_x,
+			grid_y: grid_y,
+		});
+
+		this.is_obstacle = false;
+		this.is_path = false;
+		this.is_start = false;
+		this.is_end = false;
+	}
+
+	update_color() {
+		if (this.is_start) {
+			this.color = "green";
+		} else if (this.is_end) {
+			this.color = "red";
+		} else if (this.is_obstacle) {
+			this.color = "grey";
+		} else if (this.is_path) {
+			this.color = "blue";
+		} else {
+			this.color = null;
+		}
+	}
+
+	set is_obstacle(value) {
+		this.obstacle = value;
+		this.update_color();
+		return true;
+	}
+
+	get is_obstacle() {
+		return this.obstacle != null ? this.obstacle : false;
+	}
+
+	set is_path(value) {
+		this.path = value;
+		this.update_color();
+		return true;
+	}
+
+	get is_path() {
+		return this.path != null ? this.path : false;
+	}
+
+	set is_start(value) {
+		this.start = value;
+		this.update_color();
+	}
+
+	get is_start() {
+		return this.start != null ? this.start : false;
+	}
+
+	set is_end(value) {
+		this.end = value;
+		this.update_color();
+	}
+
+	get is_end() {
+		return this.end != null ? this.end : false;
+	}
 }
 
 document.onload = load();
