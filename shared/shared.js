fa96d0a8e60c1642bd670196994cdb8e1592be64
Oscar Brunelle <oscar.brunelle@gmail.com>
2021-02-27 23:13:43 +0100

    added binary conversion functions

--- shared/shared.js
+++ shared/shared.js
@@ -213,6 +213,66 @@ function round_to_nearest(number, rounding) {
 	return (number === rounding || floor(number, rounding) % 1 === rounding) ? floor(number, rounding) : floor(number + rounding, rounding);
 }
 
+/**
+ * Returns the hexadecimal equivalent of a decimal number.
+ * Use >>> to convert to unsigned representation.
+ * @param {Number} number the decimal number
+ */
+function dec_to_hex(number) {
+	return number.toString(16);
+}
+
+/**
+ * Returns the binary equivalent of a decimal number.
+ * Use >>> to convert to unsigned representation.
+ * @param {Number} number the decimal number
+ */
+function dec_to_bin(number) {
+	return number.toString(2);
+}
+
+/**
+ * Returns the decimal equivalent of a hexadecimal number.
+ * @param {String} string the hexadecimal number
+ */
+function hex_to_dec(string) {
+	return parseInt(string, 16);
+}
+
+/**
+ * Returns the decimal equivalent of a binary number.
+ * Does not work for floats yet.
+ * @param {String} string the binary number
+ */
+function bin_to_dec(string) {
+	string = string.toString();
+	let dec = 0;
+	for (let i = string.length - 1; i >= 0; i--) {
+		let bin = parseInt(string[i]);
+		dec += bin * Math.pow(2, string.length - 1 - i);
+	}
+	return dec;
+}
+
+/**
+ * Returns the hexadecimal equivalent of a binary number.
+ * Does not work for floats yet.
+ * @param {String} string the binary number
+ */
+function bin_to_hex(string) {
+	string = string.toString();
+	let dec = "";
+	for (let i = string.length - 1; i >= 0; i -= 4) {
+		dec = dec_to_hex(bin_to_dec(string.substring(Math.max(0, i - 3), i + 1))) + dec;
+	}
+	return parseInt(dec);
+}
+
+function to_fixed_length(number, length) {
+	let string = (("0".repeat(length)) + number).toString();
+	return string.substring(string.length - length, string.length);
+}
+
 
 /* START OF SVG */
 const xmlns = "http://www.w3.org/2000/svg";
