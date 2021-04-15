bdc489e731a03ffe1e791eda79f2f7a606185479
Oscar Brunelle <oscar.brunelle@gmail.com>
2021-04-15 17:03:12 +0200

    added ui for the cards

--- projects/casino/blackjack.js
+++ projects/casino/blackjack.js
@@ -1,6 +1,9 @@
 let values = ["As", 2, 3, 4, 5, 6, 7, 8, 9, 10, "Valet", "Dame", "Roi"];
 let signs = ["Trèfle", "Coeur", "Pique", "Carreau"];
 
+const cols = [30, 50, 70];
+const rows = [25, 50, 65, 80, 100, 120, 135, 150, 175];
+
 class Card {
 	constructor(_number, _sign) {
 		this.number = _number;
@@ -23,6 +26,96 @@ class Card {
 		svgtext(this.svg, 10, 175, get_visual_sign(this.sign), this.sign + " bottom");
 		svgtext(this.svg, 90, 190, get_visual_name(this.number), this.sign + " bottom");
 		svgtext(this.svg, 90, 175, get_visual_sign(this.sign), this.sign + " bottom");
+
+		if (this.value > 9) {
+			svgtext(this.svg, cols[1], rows[4], get_visual_name(this.number), this.sign + " inner");
+		} else {
+			const ref = this;
+
+			function sign(x, y) {
+				svgtext(ref.svg, x, y, get_visual_sign(ref.sign), ref.sign + " inner" + (y > rows[4] ? " bottom" : ""));
+			}
+			switch (this.value) {
+				case 0:
+					sign(cols[1], rows[4]);
+					break;
+				case 1:
+					sign(cols[1], rows[0]);
+					sign(cols[1], rows[8]);
+					break;
+				case 2:
+					sign(cols[1], rows[0]);
+					sign(cols[1], rows[4]);
+					sign(cols[1], rows[8]);
+					break;
+				case 3:
+					sign(cols[0], rows[0]);
+					sign(cols[2], rows[0]);
+					sign(cols[0], rows[8]);
+					sign(cols[2], rows[8]);
+					break;
+				case 4:
+					sign(cols[0], rows[0]);
+					sign(cols[2], rows[0]);
+					sign(cols[1], rows[4]);
+					sign(cols[0], rows[8]);
+					sign(cols[2], rows[8]);
+					break;
+				case 5:
+					sign(cols[0], rows[0]);
+					sign(cols[2], rows[0]);
+					sign(cols[0], rows[4]);
+					sign(cols[2], rows[4]);
+					sign(cols[0], rows[8]);
+					sign(cols[2], rows[8]);
+					break;
+				case 6:
+					sign(cols[0], rows[0]);
+					sign(cols[2], rows[0]);
+					sign(cols[1], rows[2]);
+					sign(cols[0], rows[4]);
+					sign(cols[2], rows[4]);
+					sign(cols[0], rows[8]);
+					sign(cols[2], rows[8]);
+					break;
+				case 7:
+					sign(cols[0], rows[0]);
+					sign(cols[2], rows[0]);
+					sign(cols[1], rows[2]);
+					sign(cols[0], rows[4]);
+					sign(cols[2], rows[4]);
+					sign(cols[1], rows[6]);
+					sign(cols[0], rows[8]);
+					sign(cols[2], rows[8]);
+					break;
+				case 8:
+					sign(cols[0], rows[0]);
+					sign(cols[2], rows[0]);
+					sign(cols[0], rows[3]);
+					sign(cols[2], rows[3]);
+					sign(cols[1], rows[4]);
+					sign(cols[0], rows[5]);
+					sign(cols[2], rows[5]);
+					sign(cols[0], rows[8]);
+					sign(cols[2], rows[8]);
+					break;
+				case 9:
+					sign(cols[0], rows[0]);
+					sign(cols[2], rows[0]);
+					sign(cols[1], rows[1]);
+					sign(cols[0], rows[3]);
+					sign(cols[2], rows[3]);
+					sign(cols[0], rows[5]);
+					sign(cols[2], rows[5]);
+					sign(cols[1], rows[7]);
+					sign(cols[0], rows[8]);
+					sign(cols[2], rows[8]);
+					break;
+
+				default:
+					break;
+			}
+		}
 	}
 }
 
