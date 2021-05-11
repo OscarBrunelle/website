b9bdf84948ced2c6572262f060053a5d55e9ba4f
Oscar Brunelle <oscar.brunelle@gmail.com>
2021-05-12 00:11:53 +0200

    added top left right divs

--- projects/cv-creator/main.js
+++ projects/cv-creator/main.js
@@ -1,17 +1,46 @@
 "use strict"
 
 const cv = document.getElementById("cv");
+const side_top = document.getElementById("top");
+const left = document.getElementById("left");
+const right = document.getElementById("right");
+
 const sections = [
 	{
+		side: 0,
 		title: "Section 1"
 	}, {
+		side: 0,
+		title: "Section 2"
+	}, {
+		side: 0,
+		title: "Section 2"
+	}, {
+		side: 0,
+		title: "Section 2"
+	}, {
+		side: 0,
+		title: "Section 2"
+	}, {
+		side: 0,
 		title: "Section 2"
 	}
 ];
+const cv_data = {
+	first_name: "Oscar",
+	last_name: "Brunelle",
+	position: "Développeur web",
+	description: "blablabla",
+	sections: sections
+};
 
 function load() {
+	doch1(side_top, `${cv_data.first_name} ${cv_data.last_name.toUpperCase()}`);
+	docspan(side_top, cv_data.position);
+	docp(side_top, cv_data.description);
+
 	for (const section of sections) {
-		const sect = docsection(cv);
+		const sect = docsection(section.side == 0 ? left : right);
 		doch1(sect, section.title);
 	}
 }
