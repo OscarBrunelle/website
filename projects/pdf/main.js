18d59466cf5fae7fd3bb355e9f714ed8044f25dc
Oscar Brunelle <oscar.brunelle@gmail.com>
2021-05-14 12:26:12 +0200

    working pdf file

--- projects/pdf/main.js
+++ projects/pdf/main.js
@@ -1,46 +1,83 @@
 "use strict"
 
+let pdf;
 let obj_counter = 1;
-function add_obj(inp, end = true) {
-	let str = `\n${obj_counter++} 0 obj << ${inp} >>`;
-	str += end ? " endobj" : "";
+let objs = [];
+let bytes_count = 0;
+
+function add_line(str, prepend_nl = true) {
+	if (prepend_nl) {
+		pdf += "\n";
+	}
+	pdf += str;
+	bytes_count += str.length + 1;
 	return str;
 }
 
+function add_obj(inp, end = true) {
+	let str = `${obj_counter++} 0 obj << ${inp} >>`;
+	str += end ? " endobj" : "";
+	objs.push(pdf.length + 1);
+	return add_line(str);
+}
+
 function fill_rect(color, coords) {
-	return "\n0.7 0.7 1 rg\n0 0 612 792 re f";
+	add_line("0.7 0.7 1 rg");
+	return add_line("0 0 612 792 re f");
 }
 
 function add_text() {
-	return "\n0 g\nBT /F1 12 Tf 2 780 Td (Hello world) Tj ET\nBT /F1 12 Tf 2 766 Td (Second line) Tj ET";
+	add_line("0 g");
+	add_line("BT /F1 12 Tf 2 780 Td (Hello world) Tj ET");
+	return add_line("BT /F1 12 Tf 2 766 Td (Second line) Tj ET");
 }
 
 function create_pdf() {
-	let pdf = "%PDF-1.7";
-	pdf += add_obj("/Type /Catalog /Pages 2 0 R");
-	pdf += add_obj("/Type /Pages /Kids [3 0 R] /Count 1 /MediaBox [0 0 612 792]");
-	pdf += add_obj("/Type /Page /Resources 4 0 R /Parent 2 0 R /Contents 5 0 R");
-	pdf += add_obj("/Font << /F1 << /Type /Font /Subtype /Type1 /BaseFont /Helvetica >> >>");
+	pdf = "";
+	obj_counter = 1;
+	objs = [];
+	bytes_count = 0;
 
-	pdf += add_obj("/Length 167");
-	pdf += "\nstream";
-	pdf += fill_rect();
-	pdf += add_text();
-	pdf += "\nendstream";
-	pdf += "\nendobj";
+	add_line("%PDF-1.7", false);
+	add_obj("/Type /Catalog /Pages 2 0 R");
+	add_obj("/Type /Pages /Kids [3 0 R] /Count 1 /MediaBox [0 0 612 792]");
+	add_obj("/Type /Page /Resources 4 0 R /Parent 2 0 R /Contents 5 0 R");
+	add_obj("/Font << /F1 << /Type /Font /Subtype /Type1 /BaseFont /Helvetica >> >>");
 
-	pdf += "\nxref\n0 6";
-	pdf += "\n000000000 65535 f \n000000009 00000 n \n000000058 00000 n \n000000139 00000 n \n000000219 00000 n \n000000311 00000 n \ntrailer << /Size 6 /Root 1 0 R >>\n529";
+	add_obj("/Length 167", false);
+	add_line("stream");
+	fill_rect();
+	add_text();
+	add_line("endstream");
+	add_line("endobj");
+
+	const stream_bytes = bytes_count;
+	add_line("xref");
+	add_line("0 " + (objs.length + 1));
+	for (let i = 0; i < objs.length; i++) {
+		if (i == 0) {
+			add_line("000000000 65535 f ");
+		}
+		add_line(to_fixed_length(objs[i], 9) + " 00000 n ");
+	}
+
+	add_line("trailer << /Size " + (objs.length + 1) + " /Root 1 0 R >>");
+	add_line(529);//stream_bytes);
 	
-	pdf += "\nstartxref\n%%EOF";
+	add_line("startxref");
+	add_line("%%EOF");
 
 	return pdf;
 }
 
+function dl() {
+	download(pdf, "test.pdf");
+}
+
 function load() {
-	const outp = create_pdf();
-	console.log(outp);
-	download(outp, "test.pdf");
+	create_pdf();
+	console.log(pdf);
+	
 }
 
 document.onload = load();
