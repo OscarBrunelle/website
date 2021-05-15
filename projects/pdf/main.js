d74df38c683e4614feb174bc6f9f4b3ec914bb69
Oscar Brunelle <oscar.brunelle@gmail.com>
2021-05-15 12:00:32 +0200

    added pdf classes

--- projects/pdf/main.js
+++ projects/pdf/main.js
@@ -31,6 +31,66 @@ function add_text(text, pos, color = "0 g") {
 	return add_line("BT /F1 12 Tf " + pos + " Td (" + text + ") Tj ET");
 }
 
+class PdfPage {
+  constructor(){
+    this.fonts = [];
+    this.stream = [];
+  }
+  
+  add_font(name = "Helvetica"){
+    this.fonts.push(name);
+    return this;
+  }
+  
+  fill_rect(coords, color = "0 0 0") {
+  	this.stream.push(`${color} rg`);
+  	this.stream.push(`${coords} re f`);
+  	return this;
+  }
+  
+  add_text(text, pos, color = "0 g") {
+  	this.stream.push(color);
+  	this.stream.push("BT /F1 12 Tf " + pos + " Td (" + text + ") Tj ET");
+  	return this;
+  }
+}
+
+class Pdf {
+  constructor(){
+    this.bytes_count = 0;
+    this.objs = [];
+    this.obj_counter = 1;
+    this.pages = [];
+    this.doc = "%PDF-1.7";
+  }
+  
+  add_line(str, prepend_nl = true) {
+  	if (prepend_nl) {
+  		this.doc += "\n";
+  	}
+  	this.doc += str;
+  	this.bytes_count += str.length + 1;
+  	return this;
+  }
+  
+  add_obj(inp, end = true) {
+    let str = `${this.obj_counter++} 0 obj << ${inp} >>`;
+    str += end ? " endobj" : "";
+    this.objs.push(this.doc.length + 1);
+    this.add_line(str);
+    return this;
+  }
+  
+  create(){
+    return this;
+  }
+  
+  download(name = "document"){
+    download(this.doc, name);
+    return this;
+  }
+}
+
 function create_pdf() {
 	pdf = "";
 	obj_counter = 1;
