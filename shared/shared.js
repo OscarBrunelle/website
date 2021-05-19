391c01b94fadb606fddc58cb48f1149926d64a4c
Oscar Brunelle <oscar.brunelle@gmail.com>
2021-05-19 23:32:49 +0200

    ajout de la fonction getTextWidth

--- shared/shared.js
+++ shared/shared.js
@@ -33,6 +33,23 @@ class LanguageSelector {
 	}
 }
 
+/**
+ * Uses canvas.measureText to compute and return the width of the given text of given font in pixels.
+ * 
+ * @param {String} text The text to be rendered.
+ * @param {String} font The css font descriptor that text is to be rendered with (e.g. "bold 14px verdana").
+ * 
+ * @see https://stackoverflow.com/questions/118241/calculate-text-width-with-javascript/21015393#21015393
+ */
+function getTextWidth(text, font = "12px arial") {
+	// re-use canvas object for better performance
+	var canvas = getTextWidth.canvas || (getTextWidth.canvas = document.createElement("canvas"));
+	var context = canvas.getContext("2d");
+	context.font = font;
+	var metrics = context.measureText(text);
+	return metrics.width;
+}
+
 function get_cookie(cookie_name) {
 	const name = cookie_name + "=";
 	const decodedCookie = decodeURIComponent(document.cookie);
