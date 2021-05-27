cd1cdcc891adb5bae0e33cfda5b9aec4fa35d772
Oscar <oscar.brunelle@gmail.com>
2021-05-28 00:32:37 +0200

    value is now saved in cookie

--- projects/counter/main.js
+++ projects/counter/main.js
@@ -5,9 +5,13 @@ let count = 0;
 function update_count(value = 0) {
 	count += value;
 	document.getElementById("counter").innerHTML = count;
+	set_cookie("count", count);
 }
 
 function load() {
+	if (get_cookie("count") != null) {
+		count = parseInt(get_cookie("count"));
+	}
 	update_count();
 }
 
