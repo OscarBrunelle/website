7a32c61ba0d0e40db43bb91701bf6c29eb16a358
Oscar Brunelle <oscar.brunelle@gmail.com>
2021-06-16 20:52:49 +0200

    fixed positions

--- projects/bitmoji/parts.js
+++ projects/bitmoji/parts.js
@@ -6,31 +6,31 @@ const svg_dims = {
 };
 const faces_pos = {
 	x: svg_dims.w / 2,
-	y: svg_dims.h / 2
+	y: svg_dims.h
 };
 const eyes_pos = {
 	x: svg_dims.w / 2,
-	y: svg_dims.h / 2
+	y: svg_dims.h / 2 - 10
 };
 const noses_pos = {
 	x: svg_dims.w / 2,
 	y: svg_dims.h / 2
 };
-const mouth_pos = {
+const mouths_pos = {
 	x: svg_dims.w / 2,
-	y: svg_dims.h / 2
+	y: svg_dims.h / 2 + 10
 };
 const hair_pos = {
 	x: svg_dims.w / 2,
-	y: svg_dims.h / 2
+	y: svg_dims.h / 4
 };
 const hairs_pos = {
 	x: svg_dims.w / 2,
-	y: svg_dims.h / 2
+	y: svg_dims.h * 3 / 4
 };
 const clothes_pos = {
 	x: svg_dims.w / 2,
-	y: svg_dims.h / 2
+	y: svg_dims.h * 3 / 4
 };
 const accessories_pos = {
 	x: svg_dims.w / 2,
@@ -42,29 +42,29 @@ const FACES = [{
 }, ];
 
 const EYES = [{
-	"path": `M ${faces_pos.x} ${faces_pos.y} q 10 10 20 20`
+	"path": `M ${eyes_pos.x} ${eyes_pos.y} q 10 10 20 20`
 }, ];
 
 const NOSES = [{
-	"path": `M ${faces_pos.x} ${faces_pos.y} q 10 10 20 20`
+	"path": `M ${noses_pos.x} ${noses_pos.y} q 10 10 20 20`
 }, ];
 
 const MOUTHS = [{
-	"path": `M ${faces_pos.x} ${faces_pos.y} q 10 10 20 20`
+	"path": `M ${mouths_pos.x} ${mouths_pos.y} q 10 10 20 20`
 }, ];
 
 const HAIR = [{
-	"path": `M ${faces_pos.x} ${faces_pos.y} q 10 10 20 20`
+	"path": `M ${hair_pos.x} ${hair_pos.y} q 10 10 20 20`
 }, ];
 
 const HAIRS = [{
-	"path": `M ${faces_pos.x} ${faces_pos.y} q 10 10 20 20`
+	"path": `M ${hairs_pos.x} ${hairs_pos.y} q 10 10 20 20`
 }, ];
 
 const CLOTHES = [{
-	"path": `M ${faces_pos.x} ${faces_pos.y} q 10 10 20 20`
+	"path": `M ${clothes_pos.x} ${clothes_pos.y} q 10 10 20 20`
 }, ];
 
 const ACCESSORIES = [{
-	"path": `M ${faces_pos.x} ${faces_pos.y} q 10 10 20 20`
+	"path": `M ${accessories_pos.x} ${accessories_pos.y} q 10 10 20 20`
 }, ];
