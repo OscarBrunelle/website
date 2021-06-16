001dc82b38da6bdb40a634add7f50ac2b96e9ece
Oscar Brunelle <oscar.brunelle@gmail.com>
2021-06-16 20:41:29 +0200

    bitmoji initial commit

---
+++ projects/bitmoji/main.js
@@ -0,0 +1,7 @@
+"use strict"
+
+function load() {
+
+}
+
+document.onload = load();
---
+++ projects/bitmoji/parts.js
@@ -0,0 +1,70 @@
+"use strict"
+
+const svg_dims = {
+	w: 100,
+	h: 100
+};
+const faces_pos = {
+	x: svg_dims.w / 2,
+	y: svg_dims.h / 2
+};
+const eyes_pos = {
+	x: svg_dims.w / 2,
+	y: svg_dims.h / 2
+};
+const noses_pos = {
+	x: svg_dims.w / 2,
+	y: svg_dims.h / 2
+};
+const mouth_pos = {
+	x: svg_dims.w / 2,
+	y: svg_dims.h / 2
+};
+const hair_pos = {
+	x: svg_dims.w / 2,
+	y: svg_dims.h / 2
+};
+const hairs_pos = {
+	x: svg_dims.w / 2,
+	y: svg_dims.h / 2
+};
+const clothes_pos = {
+	x: svg_dims.w / 2,
+	y: svg_dims.h / 2
+};
+const accessories_pos = {
+	x: svg_dims.w / 2,
+	y: svg_dims.h / 2
+};
+
+const FACES = [{
+	"path": `M ${faces_pos.x} ${faces_pos.y} q 10 10 20 20`
+}, ];
+
+const EYES = [{
+	"path": `M ${faces_pos.x} ${faces_pos.y} q 10 10 20 20`
+}, ];
+
+const NOSES = [{
+	"path": `M ${faces_pos.x} ${faces_pos.y} q 10 10 20 20`
+}, ];
+
+const MOUTHS = [{
+	"path": `M ${faces_pos.x} ${faces_pos.y} q 10 10 20 20`
+}, ];
+
+const HAIR = [{
+	"path": `M ${faces_pos.x} ${faces_pos.y} q 10 10 20 20`
+}, ];
+
+const HAIRS = [{
+	"path": `M ${faces_pos.x} ${faces_pos.y} q 10 10 20 20`
+}, ];
+
+const CLOTHES = [{
+	"path": `M ${faces_pos.x} ${faces_pos.y} q 10 10 20 20`
+}, ];
+
+const ACCESSORIES = [{
+	"path": `M ${faces_pos.x} ${faces_pos.y} q 10 10 20 20`
+}, ];
