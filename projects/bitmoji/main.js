539095cc2a73baf3f3634f6eeeaf7a17211ba377
Oscar Brunelle <oscar.brunelle@gmail.com>
2021-06-16 20:49:22 +0200

    each part is drawn

--- projects/bitmoji/main.js
+++ projects/bitmoji/main.js
@@ -1,7 +1,12 @@
 "use strict"
 
-function load() {
+const svg = document.getElementById("svg");
+const parts = [FACES, EYES, NOSES, MOUTHS, HAIR, HAIRS, CLOTHES, ACCESSORIES];
 
+function load() {
+	for (const part of parts) {
+		svgpath(svg, part[0].path);
+	}
 }
 
 document.onload = load();
