32953436ef192154a30da247f276759cbbf5d75e
Oscar Brunelle <oscar.brunelle@gmail.com>
2021-09-06 22:55:46 +0200

    Déplacement du job_title en premier dans le titre

--- projects/cv-creator/content.js
+++ projects/cv-creator/content.js
@@ -8,13 +8,13 @@ function get_date(year, month = null) {
 
 function add_job_title(data, parent) {
 	const title_container = doch3(parent, null, "title");
+	if (data.job_title != null) docspan(title_container, data.job_title, "job_title");
 	if (data.date_year != null) docspan(title_container, get_date(data.date_year, data.date_month), "date-start");
 	if (data.duration == -1) {
 		docspan(title_container, "Aujourd'hui", "date-end");
 	} else if (data.duration != null) {
 		docspan(title_container, get_date(data.date_year, data.date_month + data.duration - 1), "date-end");
 	}
-	if (data.job_title != null) docspan(title_container, data.job_title, "job_title");
 	if (data.country != null) docspan(title_container, data.country, "country");
 	if (data.employer != null) doch4(parent, data.employer, "subtitle");
 }
