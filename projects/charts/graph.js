0d7def526f5e572aefb600eb2b5d9a4674fbaf02
Oscar Brunelle <oscar.brunelle@gmail.com>
2021-09-23 18:13:22 +0200

    Added data points, renamed file, and added sort function

--- projects/charts/graph.js
+++ projects/charts/graph.js
@@ -1,10 +1,17 @@
 const size = 100;
+const padd_perc = 0.1;
 
 function format_data(data) {
 	for (const d of data) {
 		d.date = new Date(d.date);
 	}
 	return data;
+}
+
+function sort_data(data) {
+	return data.sort(function (a, b) {
+		return a - b;
+	});
 }
 
 function get_index_range(data) {
@@ -26,7 +33,7 @@ function get_value_range(data) {
 }
 
 function get_pos(value, min_range, max_range) {
-	return (value - min_range) / (max_range - min_range) * 100 * 0.8 + (0.1 * size);
+	return (value - min_range) / (max_range - min_range) * size * (1 - padd_perc * 2) + (padd_perc * size);
 }
 
 function create_graph(data) {
@@ -35,6 +42,7 @@ function create_graph(data) {
 	// }
 	data = data["set1"];
 	data = format_data(data);
+	data = sort_data(data);
 	let [min_index, max_index] = get_index_range(data);
 	let [min_value, max_value] = get_value_range(data);
 	const svg = docsvg(document.getElementById("main"), `0 0 ${size} ${size}`, "graph");
--- projects/charts/main.js
+++ projects/charts/main.js
@@ -3,15 +3,31 @@
 let template_data = {
 	"set1": [
 		{
-			date: "2021-09-23T16:29:00",
+			date: "2021-09-23T16:20:00",
 			value: "13",
 			label: "labeltest"
 		}, {
-			date: "2021-09-23T16:29:30",
+			date: "2021-09-23T16:55:30",
 			value: "17",
 			label: "labeltest2"
 		}, {
-			date: "2021-09-23T16:30:00",
+			date: "2021-09-23T17:04:00",
+			value: "15",
+			label: "labeltest2"
+		}, {
+			date: "2021-09-23T16:45:30",
+			value: "17",
+			label: "labeltest2"
+		}, {
+			date: "2021-09-23T17:54:00",
+			value: "15",
+			label: "labeltest2"
+		}, {
+			date: "2021-09-23T16:15:30",
+			value: "17",
+			label: "labeltest2"
+		}, {
+			date: "2021-09-23T17:34:00",
 			value: "15",
 			label: "labeltest2"
 		}
