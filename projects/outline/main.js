ea51fa149d8abe21536e73e455094e06ebe256ca
Oscar Brunelle <oscar.brunelle@gmail.com>
2021-09-23 17:35:33 +0200

    Outline initial commit

---
+++ projects/outline/main.js
@@ -0,0 +1,19 @@
+"use strict"
+
+/*
+Plan:
+
+show initial image
+detecter et dessiner contours
+trouver coins chaque formes
+trouver trucs qui sortent
+matcher pieces
+reconstituer puzzle
+montrer puzzle solved
+animation solve puzzle
+*/
+
+function load() {
+}
+
+document.onload = load();
---
+++ shared/js/canvas.js
@@ -0,0 +1,22 @@
+"use strict"
+
+class Canvas {
+	constructor(parent_selector, _width, _height, id = "game_canvas") {
+		let canvas = document.getElementById(id);
+		if (canvas == null) {
+			canvas = document.createElement("canvas");
+			document.querySelector(parent_selector).appendChild(canvas);
+		}
+		this.canvas = canvas;
+		this.canvas.tabIndex = 1;
+		this.id = id;
+		this.width = _width;
+		this.height = _height;
+		this.x = this.width / 2;
+		this.y = this.height / 2;
+
+		this.context = this.canvas.getContext("2d");
+		this.context.textAlign = "center";
+		this.context.textBaseline = "middle";
+	}
+}
