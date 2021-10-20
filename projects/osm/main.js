a2270b40e3b2130a132884ed6d8be5cd4a6c36ed
Oscar Brunelle <oscar.brunelle@gmail.com>
2021-10-21 01:35:01 +0200

    osm: Added get_current_position function

--- projects/osm/main.js
+++ projects/osm/main.js
@@ -4,16 +4,41 @@ var input = document.getElementById("myFile");
 var output = document.getElementById("output");
 
 input.addEventListener("change", function () {
-  if (this.files && this.files[0]) {
-    var myFile = this.files[0];
-    var reader = new FileReader();
-    
-    reader.addEventListener("loadend", read_file);
-    
-    reader.readAsArrayBuffer(myFile);
-  }   
+	if (this.files && this.files[0]) {
+		var myFile = this.files[0];
+		var reader = new FileReader();
+
+		reader.addEventListener("loadend", read_file);
+
+		reader.readAsArrayBuffer(myFile);
+	}
 });
 
-function load() {}
+function get_current_position() {
+	var options = {
+		enableHighAccuracy: true,
+		timeout: 5000,
+		maximumAge: 0
+	};
+
+	function success(pos) {
+		var crd = pos.coords;
+
+		console.log('Votre position actuelle est :');
+		console.log(`Latitude : ${crd.latitude}`);
+		console.log(`Longitude : ${crd.longitude}`);
+		console.log(`La précision est de ${crd.accuracy} mètres.`);
+	}
+
+	function error(err) {
+		console.warn(`ERREUR (${err.code}): ${err.message}`);
+	}
+
+	navigator.geolocation.getCurrentPosition(success, error, options);
+}
+
+function load() {
+	// get_current_position();
+}
 
 document.onload = load();
