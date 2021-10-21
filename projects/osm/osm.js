cb60d499cb6ec127f9dbf794809cae8ee97866f9
Oscar Brunelle <oscar.brunelle@gmail.com>
2021-10-21 20:44:20 +0200

    OSM: Moved file processing to workers, and added progress percentage
    Takes 95s to process the whole file, outputing an array of 3637734 nodes

--- projects/osm/osm.js
+++ projects/osm/osm.js
@@ -1,123 +1,34 @@
 "use strict"
 
-const block_size = 1000;
-const max_loops = 20000;
-
-let number_workers = 2;
-let worker_index = 0;
-let workers = [];
 let bounds = {};
 let nodes = [];
+let number_workers = 8;
+let workers = [];
 
-function add_data_element(xml_thing, data) {
-	switch (xml_thing) {
-		case "bounds":
-			let attributes = extract_attributes(data);
-			for (const att of attributes) {
-				bounds[att.name] = att.value;
-			}
-			break;
-		case "node":
-			add_node(data);
-			break;
-	}
-}
-
-function extract_attributes(line) {
-	let attributes = [];
-
-	for (const attribute of line.trim().split(" ").slice(1)) {
-		let n = attribute.split("=")[0];
-		if (n.includes("\"")) n = n.split("\"", 2)[1].split("\"", 1)[0];
-		let v = attribute.split("=")[1];
-		if (v.includes("\"")) v = v.split("\"", 2)[1].split("\"", 1)[0];
-		attributes.push({
-			"name": n,
-			"value": v
-		});
-	}
-
-	return attributes;
-}
-
-function extract_tags(line) {
-	let n = line.split("k=\"", 2)[1].split("\" ")[0].split("\"/>")[0];
-	let v = line.split("v=\"", 2)[1].split("\" ")[0].split("\"/>")[0];
-	return {
-		"name": n,
-		"value": v
-	};
-}
-
-function add_node(data_string) {
-	let node = {};
-	let attributes = ["id", "lat", "lon"];
-	let tags = ["amenity", "name"];
-
-	let lines = data_string.trim().split("\n");
-	for (let line_index in lines) {
-		const line = lines[line_index];
-		if (lines.length > 2 && line_index >= lines.length - 2) {
-			continue;
-		} else if (line_index == 0) {
-			let line_attributes = extract_attributes(line);
-			for (const line_tag of line_attributes) {
-				if (attributes.includes(line_tag.name)) {
-					node[line_tag.name] = line_tag.value;
-				}
-			}
-		} else {
-			let tag = extract_tags(line);
-			if (tags.includes(tag.name)) node[tag.name] = tag.value;
-		}
-	}
-
-	nodes.push(node);
-	// workers[worker_index].postMessage({
-	// 	"canvas": canvas.canvas,
-	// 	"node": node,
-	// 	"bounds": bounds
-	// });
-	worker_index = (worker_index + 1) % number_workers;
-
-	return node;
-}
+let chunk_size = 100000;
+const chunks_to_process = 10000;
 
 function read_file(e) {
 	const buffer = new Uint8Array(e.target.result);
 	const data_length = e.target.result.byteLength;
-	let byte_index = 0;
-	let while_count = 0;
-	while (byte_index < data_length && while_count++ < max_loops) {
-		let snippet = new TextDecoder('utf-8').decode(buffer.slice(byte_index, Math.min(data_length, byte_index + block_size)));
-		let start_symbol_i = snippet.indexOf("<");
-		const xml_thing = snippet.substring(start_symbol_i + 1, start_symbol_i + 10).split(" ")[0];
-		switch (xml_thing) {
-			case "?xml":
-				byte_index = 39;
-				break;
-			case "osm":
-				byte_index = 86;
-				break;
-			case "bounds":
-			case "node":
-				let end_symbol_i = snippet.slice(start_symbol_i).indexOf(">") + snippet.slice(0, start_symbol_i).length;
-				if (end_symbol_i == -1) console.error("Could not find closing bracket");
-				if (snippet.substring(end_symbol_i - 1, end_symbol_i) != "/") {
-					end_symbol_i = snippet.indexOf(`</${xml_thing}>`) + `</${xml_thing}>`.length - 1;
-				}
-				let string = snippet.substring(start_symbol_i, end_symbol_i + 1);
-				byte_index += (new TextEncoder().encode(snippet.substring(0, end_symbol_i + 1))).length;
-				add_data_element(xml_thing, string.trim());
-				break;
-			case "/node>":
-			case "/node>\n":
-			case "tag":
-				byte_index += 5;
-				break;
-			default:
-				console.error(`Error: does not know xml symbol: '${xml_thing}'.`);
-				break;
+	chunk_size = Math.min(chunk_size, data_length / number_workers);
+
+	console.time("workers");
+	let chunk_index = 0;
+	for (const worker of workers) {
+		worker.onmessage = function (e) {
+			document.getElementById("progress").innerText = parseInt(chunk_index * chunk_size / data_length * 100);
+			nodes = nodes.concat(e.data);
+			if (chunk_index < data_length / chunk_size && chunk_index < chunks_to_process) {
+				let start_chunk = chunk_size * (chunk_index++);
+				worker.postMessage([buffer.slice(start_chunk, start_chunk + chunk_size), bounds]);
+			} else {
+				console.timeLog("workers", nodes);
+			}
+		};
+		if (chunk_index < data_length / chunk_size && chunk_index < chunks_to_process) {
+			let start_chunk = chunk_size * (chunk_index++);
+			worker.postMessage([buffer.slice(start_chunk, start_chunk + chunk_size), bounds]);
 		}
 	}
 }
@@ -128,7 +39,7 @@ function inter_perc(value, min, max) {
 
 function load_file(e) {
 	for (let i = 0; i < number_workers; i++) {
-		// workers.push(new Worker('worker.js'));
+		workers.push(new Worker('worker.js'));
 	}
 	console.info("Processing OSM file...");
 	read_file(e);
--- projects/osm/worker.js
+++ projects/osm/worker.js
@@ -1,13 +1,145 @@
 "use strict"
 
+let bounds = {};
+let nodes = [];
+
+function reset_worker() {
+	bounds = {};
+	nodes = [];
+}
+
+function add_data_element(xml_thing, data) {
+	switch (xml_thing) {
+		case "bounds":
+			let attributes = extract_attributes(data);
+			for (const att of attributes) {
+				bounds[att.name] = att.value;
+			}
+			break;
+		case "node":
+			add_node(data);
+			break;
+	}
+}
+
+function extract_attributes(line) {
+	let attributes = [];
+
+	for (const attribute of line.trim().split(" ").slice(1)) {
+		let n = attribute.split("=")[0];
+		if (n.includes("\"")) n = n.split("\"", 2)[1].split("\"", 1)[0];
+		let v = attribute.split("=")[1];
+		if (v.includes("\"")) v = v.split("\"", 2)[1].split("\"", 1)[0];
+		attributes.push({
+			"name": n,
+			"value": v
+		});
+	}
+
+	return attributes;
+}
+
+function extract_tags(line) {
+	let n = line.split("k=\"", 2)[1].split("\" ")[0].split("\"/>")[0];
+	let v = line.split("v=\"", 2)[1].split("\" ")[0].split("\"/>")[0];
+	return {
+		"name": n,
+		"value": v
+	};
+}
+
+function add_node(data_string) {
+	let node = {};
+	let attributes = ["id", "lat", "lon"];
+	let tags = ["amenity", "name"];
+
+	let lines = data_string.trim().split("\n");
+	for (let line_index in lines) {
+		const line = lines[line_index];
+		if (lines.length > 2 && line_index >= lines.length - 2) {
+			continue;
+		} else if (line_index == 0) {
+			let line_attributes = extract_attributes(line);
+			for (const line_tag of line_attributes) {
+				if (attributes.includes(line_tag.name)) {
+					node[line_tag.name] = line_tag.value;
+				}
+			}
+		} else {
+			let tag = extract_tags(line);
+			if (tags.includes(tag.name)) node[tag.name] = tag.value;
+		}
+	}
+
+	nodes.push(node);
+	// workers[worker_index].postMessage({
+	// 	"canvas": canvas.canvas,
+	// 	"node": node,
+	// 	"bounds": bounds
+	// });
+
+	return node;
+}
+
 function inter_perc(value, min, max) {
 	return (value - min) / (max - min);
 }
 
-addEventListener('message', function(e) {
-	let canvas = e.data.canvas;
-	let context = canvas.getContext("2d");
-	let node = e.data.node;
-	let bounds = e.data.bounds;
-	context.fillRect(inter_perc(node.lat, bounds.minlat, bounds.maxlat) * 600, inter_perc(node.lon, bounds.minlon, bounds.maxlon) * 600, 1, 1);
+const block_size = 500;
+const max_loops = 1000;
+
+addEventListener("message", function (e) {
+	reset_worker();
+
+	bounds = e.data[1];
+
+	const buffer = e.data[0];
+	const data_length = buffer.length;
+
+	let byte_index = 0;
+	let while_count = 0;
+	break_flag:
+	while (byte_index < data_length && while_count++ < max_loops) {
+		let snippet = new TextDecoder('utf-8').decode(buffer.slice(byte_index, Math.min(data_length, byte_index + block_size)));
+		let start_symbol_i = snippet.indexOf("<");
+		if (start_symbol_i == -1) break;
+		const xml_thing = snippet.substring(start_symbol_i + 1, start_symbol_i + 10).split(" ")[0];
+		switch (xml_thing) {
+			case "?xml":
+				byte_index = 39;
+				break;
+			case "osm":
+				byte_index = 86;
+				break;
+			case "bounds":
+			case "node":
+				let end_symbol_i = snippet.slice(start_symbol_i).indexOf(">") + snippet.slice(0, start_symbol_i).length;
+				if (end_symbol_i == -1) {
+					// console.error("Could not find closing bracket");
+					break break_flag;
+				}
+				if (snippet.substring(end_symbol_i - 1, end_symbol_i) != "/") {
+					end_symbol_i = snippet.indexOf(`</${xml_thing}>`);
+					if (end_symbol_i == -1) {
+						// console.error(`Could not find matching closing bracket: </${xml_thing}>`);
+						break break_flag;
+					}
+					end_symbol_i += `</${xml_thing}>`.length - 1;
+				}
+				let string = snippet.substring(start_symbol_i, end_symbol_i + 1);
+				byte_index += (new TextEncoder().encode(snippet.substring(0, end_symbol_i + 1))).length;
+				add_data_element(xml_thing, string.trim());
+				break;
+			case "/node>":
+			case "/node>\n":
+			case "tag":
+				byte_index += 8;
+				break;
+			default:
+				// console.error(`Error: does not know xml symbol: '${xml_thing}'.`);
+				break break_flag;
+		}
+	}
+
+	this.postMessage(nodes);
 }, false);
