a7e85bb619174203d73de390d69d6b6925a4a40b
Oscar Brunelle <oscar.brunelle@gmail.com>
2021-10-21 21:16:43 +0200

    OSM: Added real-time drawing on the map

--- projects/osm/map.js
+++ projects/osm/map.js
@@ -9,3 +9,9 @@ function create_map_visuals() {
 		canvas.rect(inter_perc(node.lat, bounds.minlat, bounds.maxlat) * w, inter_perc(node.lon, bounds.minlon, bounds.maxlon) * h, 1, 1);
 	}
 }
+
+function add_data_points(data_points) {
+	for (const d of data_points) {
+		canvas.rect(inter_perc(d.lat, bounds.minlat, bounds.maxlat) * w, inter_perc(d.lon, bounds.minlon, bounds.maxlon) * h, 1, 1);
+	}
+}
--- projects/osm/osm.js
+++ projects/osm/osm.js
@@ -1,12 +1,25 @@
 "use strict"
 
-let bounds = {};
+let bounds = {
+	minlat: "41.31103",
+	minlon: "8.317882",
+	maxlat: "43.16589",
+	maxlon: "9.751243"
+};
 let nodes = [];
-let number_workers = 8;
+let number_workers = 4;
 let workers = [];
 
 let chunk_size = 100000;
-const chunks_to_process = 1000;
+const chunks_to_process = 10000;
+
+function end_computing() {
+	for (const worker of workers) {
+		worker.terminate();
+	}
+	workers = [];
+	// create_map_visuals();
+}
 
 function read_file(e) {
 	const buffer = new Uint8Array(e.target.result);
@@ -15,20 +28,26 @@ function read_file(e) {
 
 	console.time("workers");
 	let chunk_index = 0;
+	let workers_active = 0;
 	for (const worker of workers) {
 		worker.onmessage = function (e) {
+			workers_active--;
+			add_data_points(e.data);
 			let perc = parseInt(chunk_index * chunk_size / data_length * 100);
 			document.getElementById("progress-perc").innerText = perc + " %";
 			document.getElementById("progress").style.width = perc + "%";
 			nodes = nodes.concat(e.data);
 			if (chunk_index < data_length / chunk_size && chunk_index < chunks_to_process) {
+				workers_active++;
 				let start_chunk = chunk_size * (chunk_index++);
 				worker.postMessage([buffer.slice(start_chunk, start_chunk + chunk_size), bounds]);
-			} else {
+			} else if (workers_active == 0) {
 				console.timeLog("workers", nodes);
+				end_computing();
 			}
 		};
 		if (chunk_index < data_length / chunk_size && chunk_index < chunks_to_process) {
+			workers_active++;
 			let start_chunk = chunk_size * (chunk_index++);
 			worker.postMessage([buffer.slice(start_chunk, start_chunk + chunk_size), bounds]);
 		}
