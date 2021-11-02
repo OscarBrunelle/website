6707d9c304561024e79c648304a2fa97258b500b
Oscar Brunelle <oscar.brunelle@gmail.com>
2021-11-02 16:15:25 +0100

    OSM: Moved worker code to clean

--- projects/osm/worker.js
+++ projects/osm/worker.js
@@ -1,20 +1,46 @@
 "use strict"
 
+// Constants (used in process_buffer function)
+const block_size = 500;
+const max_loops = 1000;
+
+// Returned variables
 let bounds = {};
 let nodes = [];
 let ways = [];
 
+// The attributes we're looking for in the xml file
 let attributes = ["id", "lat", "lon"];
 let tags = ["amenity", "name", "building"];
 
+// Entry point of the worker
+addEventListener("message", function (e) {
+	reset_worker();
+
+	bounds = e.data[1];
+
+	const buffer = e.data[0];
+	process_buffer(buffer);
+
+	this.postMessage([bounds, nodes, ways]);
+}, false);
+
+/**
+ * Empties the return values.
+ */
 function reset_worker() {
 	bounds = {};
 	nodes = [];
 	ways = [];
 }
 
-function add_data_element(xml_thing, data) {
-	switch (xml_thing) {
+/**
+ * 
+ * @param {String} xml_tag The xml tag at the beggining of the line
+ * @param {String} data The xml data to parse
+ */
+function add_data_element(xml_tag, data) {
+	switch (xml_tag) {
 		case "bounds":
 			let attributes = extract_attributes(data);
 			for (const att of attributes) {
@@ -118,66 +144,52 @@ function inter_perc(value, min, max) {
 	return (value - min) / (max - min);
 }
 
-const block_size = 500;
-const max_loops = 1000;
-
 function process_buffer(buffer) {
 	const data_length = buffer.length;
 
 	let byte_index = 0;
 	let while_count = 0;
 	break_flag:
-	while (byte_index < data_length && while_count++ < max_loops) {
-		let snippet = new TextDecoder('utf-8').decode(buffer.slice(byte_index, Math.min(data_length, byte_index + block_size)));
-		let start_symbol_i = snippet.indexOf("<");
-		if (start_symbol_i == -1) break;
-		const xml_thing = snippet.substring(start_symbol_i + 1, start_symbol_i + 10).split(" ")[0];
-		switch (xml_thing) {
-			case "?xml":
-				byte_index = 39;
-				break;
-			case "osm":
-				byte_index = 86;
-				break;
-			case "bounds":
-			case "node":
-			case "way":
-				let end_symbol_i = snippet.slice(start_symbol_i).indexOf(">") + snippet.slice(0, start_symbol_i).length;
-				if (end_symbol_i == -1) {
-					// console.error("Could not find closing bracket");
-					break break_flag;
-				}
-				if (snippet.substring(end_symbol_i - 1, end_symbol_i) != "/") {
-					end_symbol_i = snippet.indexOf(`</${xml_thing}>`);
+		while (byte_index < data_length && while_count++ < max_loops) {
+			let snippet = new TextDecoder('utf-8').decode(buffer.slice(byte_index, Math.min(data_length, byte_index + block_size)));
+			let start_symbol_i = snippet.indexOf("<");
+			if (start_symbol_i == -1) break;
+			const xml_tag = snippet.substring(start_symbol_i + 1, start_symbol_i + 10).split(" ")[0];
+			switch (xml_tag) {
+				case "?xml":
+					byte_index = 39;
+					break;
+				case "osm":
+					byte_index = 86;
+					break;
+				case "bounds":
+				case "node":
+				case "way":
+					let end_symbol_i = snippet.slice(start_symbol_i).indexOf(">") + snippet.slice(0, start_symbol_i).length;
 					if (end_symbol_i == -1) {
-						// console.error(`Could not find matching closing bracket: </${xml_thing}>`);
+						// console.error("Could not find closing bracket");
 						break break_flag;
 					}
-					end_symbol_i += `</${xml_thing}>`.length - 1;
-				}
-				let string = snippet.substring(start_symbol_i, end_symbol_i + 1);
-				byte_index += (new TextEncoder().encode(snippet.substring(0, end_symbol_i + 1))).length;
-				add_data_element(xml_thing, string.trim());
-				break;
-			case "/node>":
-			case "/node>\n":
-			case "tag":
-				byte_index += 8;
-				break;
-			default:
-				// console.error(`Error: does not know xml symbol: '${xml_thing}'.`);
-				break break_flag;
+					if (snippet.substring(end_symbol_i - 1, end_symbol_i) != "/") {
+						end_symbol_i = snippet.indexOf(`</${xml_tag}>`);
+						if (end_symbol_i == -1) {
+							// console.error(`Could not find matching closing bracket: </${xml_tag}>`);
+							break break_flag;
+						}
+						end_symbol_i += `</${xml_tag}>`.length - 1;
+					}
+					let string = snippet.substring(start_symbol_i, end_symbol_i + 1);
+					byte_index += (new TextEncoder().encode(snippet.substring(0, end_symbol_i + 1))).length;
+					add_data_element(xml_tag, string.trim());
+					break;
+				case "/node>":
+				case "/node>\n":
+				case "tag":
+					byte_index += 8;
+					break;
+				default:
+					// console.error(`Error: does not know xml symbol: '${xml_tag}'.`);
+					break break_flag;
+			}
 		}
-	}
 }
-
-addEventListener("message", function (e) {
-	reset_worker();
-
-	bounds = e.data[1];
-
-	const buffer = e.data[0];
-	process_buffer(buffer);
-
-	this.postMessage([bounds, nodes, ways]);
-}, false);
