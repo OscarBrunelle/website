91460a2bef3623f71654c77d058b3bfb6d0ad429
Oscar Brunelle <oscar.brunelle@gmail.com>
2021-11-02 17:40:58 +0100

    osm: fixed drawing function

--- projects/osm/map.js
+++ projects/osm/map.js
@@ -6,7 +6,7 @@ let canvas = new Canvas("#main", w, h, "map");
 
 function create_map_visuals() {
 	for (const node of nodes) {
-		canvas.rect(inter_perc(node.lat, bounds.minlat, bounds.maxlat) * w, inter_perc(node.lon, bounds.minlon, bounds.maxlon) * h, 1, 1);
+		canvas.rect((inter_perc(node.lon, bounds.minlon, bounds.maxlon) * h), w - inter_perc(node.lat, bounds.minlat, bounds.maxlat) * w, 1, 1);
 	}
 }
 
