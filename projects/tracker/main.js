ffad3032ac9e1df3fe010afd89c8ff5aa114ca06
Oscar Brunelle <oscar.brunelle@gmail.com>
2021-11-28 20:57:24 +0100

    Updated the way data is stored and added fields to data

--- projects/tracker/main.js
+++ projects/tracker/main.js
@@ -3,7 +3,7 @@
 const form = document.querySelector("main form");
 const add_item_input = document.querySelector("input.add_item");
 
-let possible_values = ["test", "valuetest", "valteslue", "yay", "nope", "œuf"];
+let possible_values = [];
 
 function send_item(e) {
 	e.preventDefault();
@@ -12,11 +12,16 @@ function send_item(e) {
 	add_item_input.value = "";
 	add_suggestions_for("");
 
-	let current_inventory = get_cookie("inventory");
+	let current_inventory = get_cookie("inventory_history");
 	if (current_inventory != null && current_inventory != "") current_inventory = JSON.parse(current_inventory);
 	if (!Array.isArray(current_inventory)) current_inventory = [];
-	current_inventory.push(suggestion);
-	set_cookie("inventory", JSON.stringify(current_inventory));
+	if (current_inventory[suggestion] == null) current_inventory[suggestion] = [];
+	current_inventory[suggestion].push({
+		"date": document.getElementById("consumed_date").value,
+		"moment": document.getElementById("consumed_moment").value,
+		"quantity": document.getElementById("item_quantity").value
+	});
+	set_cookie("inventory_history", JSON.stringify(current_inventory));
 	console.table(current_inventory);
 }
 
@@ -62,7 +67,11 @@ function add_suggestions_for(search_term) {
 	for (const possible_value of possible_values) {
 		if (format_search(possible_value).includes(search_term)) {
 			suggestions.push(possible_value);
-			docli(suggestionsContainer, possible_value);
+			let li = docli(suggestionsContainer, possible_value);
+			li.addEventListener("click", function(e) {
+				add_item_input.value = li.innerText;
+				add_suggestions_for(add_item_input.value);
+			});
 		}
 	}
 	if (suggestions.length > 0) {
@@ -76,12 +85,23 @@ function update_item_suggestions(e) {
 	add_suggestions_for(search);
 }
 
+function get_possible_values() {
+	possible_values = [];
+	get_csv("real_products.csv", (data) => {
+		console.log(data);
+		for (const line of data) {
+			possible_values.push(line.name);
+		}
+	});
+	add_suggestions_for("");
+}
+
 function load() {
-	// document.getElementById("consumed_date").valueAsDate = new Date();
+	get_possible_values();
+	document.getElementById("consumed_date").valueAsDate = new Date();
 	form.addEventListener("submit", send_item);
 	add_item_input.addEventListener("keydown", select_suggestion);
 	add_item_input.addEventListener("input", update_item_suggestions);
-	add_suggestions_for("");
 }
 
 document.onload = load();
--- projects/tracker/options/main.js
+++ projects/tracker/options/main.js
@@ -3,7 +3,7 @@
 function empty_inventory(e) {
 	let accept = confirm("Voulez-vous vraiment vider votre inventaire ?\nCette action est irréversible.");
 	if (accept) {
-		expire_cookie("inventory");
+		expire_cookie("inventory_history");
 		console.info("Inventaire vidé.")
 	}
 }
