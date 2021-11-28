0817361a5e0ed9b128f5d240c8ca8c2276edbef6
Oscar Brunelle <oscar.brunelle@gmail.com>
2021-11-28 21:26:38 +0100

    Fixed bug with searched, get items from csv, added item unit

--- projects/tracker/main.js
+++ projects/tracker/main.js
@@ -8,18 +8,20 @@ let possible_values = [];
 function send_item(e) {
 	e.preventDefault();
 	if (suggestions.length == 0) return;
-	const suggestion = suggestions[selected_suggestion];
+	const suggestion_data = suggestions[selected_suggestion];
+	const suggestion_name = suggestion_data["name"];
 	add_item_input.value = "";
 	add_suggestions_for("");
 
 	let current_inventory = get_cookie("inventory_history");
 	if (current_inventory != null && current_inventory != "") current_inventory = JSON.parse(current_inventory);
 	if (!Array.isArray(current_inventory)) current_inventory = [];
-	if (current_inventory[suggestion] == null) current_inventory[suggestion] = [];
-	current_inventory[suggestion].push({
+	if (current_inventory[suggestion_name] == null) current_inventory[suggestion_name] = [];
+	current_inventory[suggestion_name].push({
 		"date": document.getElementById("consumed_date").value,
 		"moment": document.getElementById("consumed_moment").value,
-		"quantity": document.getElementById("item_quantity").value
+		"quantity": document.getElementById("item_quantity").value,
+		"data": suggestion_data
 	});
 	set_cookie("inventory_history", JSON.stringify(current_inventory));
 	console.table(current_inventory);
@@ -35,17 +37,20 @@ function between(value, min, max) {
 	return Math.max(Math.min(value, max), min);
 }
 
-function select_suggestion(e) {
-	if (e.key == "ArrowDown") {
+function select_suggestion(e = null) {
+	if (e == null) {} else if (e.key == "ArrowDown") {
 		e.preventDefault();
 		selected_suggestion = between(selected_suggestion + 1, 0, suggestions.length - 1);
 	} else if (e.key == "ArrowUp") {
 		e.preventDefault();
 		selected_suggestion = between(selected_suggestion - 1, 0, suggestions.length - 1);
+	} else if (e.key == "Backspace") {
+		return update_item_suggestions(e);
 	}
 	for (const suggestion_i in suggestions) {
 		if (suggestion_i == selected_suggestion) {
 			document.querySelectorAll(".add_item_suggestions li")[suggestion_i].classList.add("selected");
+			document.getElementById("item_unit").innerText = suggestions[suggestion_i]["unit"];
 		} else {
 			document.querySelectorAll(".add_item_suggestions li")[suggestion_i].classList.remove("selected");
 		}
@@ -64,36 +69,40 @@ let selected_suggestion = 0;
 function add_suggestions_for(search_term) {
 	empty_suggestions();
 	let suggestionsContainer = document.querySelector(".add_item_suggestions");
-	for (const possible_value of possible_values) {
+	for (const possible_value_data of possible_values) {
+		let possible_value = possible_value_data["name"];
 		if (format_search(possible_value).includes(search_term)) {
-			suggestions.push(possible_value);
+			suggestions.push(possible_value_data);
 			let li = docli(suggestionsContainer, possible_value);
 			li.addEventListener("click", function(e) {
 				add_item_input.value = li.innerText;
 				add_suggestions_for(add_item_input.value);
+				selected_suggestion = 0;
+				select_suggestion();
 			});
 		}
 	}
 	if (suggestions.length > 0) {
-		document.querySelectorAll(".add_item_suggestions li")[0].classList.add("selected");
+		select_suggestion();
 	}
 }
 
-function update_item_suggestions(e) {
-	if (e.key == "Enter") return send_item();
-	let search = format_search(e.target.value);
+function update_item_suggestions(e = null) {
+	if (e != null && e.key == "Enter") return send_item();
+	let search = format_search(add_item_input.value);
 	add_suggestions_for(search);
 }
 
 function get_possible_values() {
 	possible_values = [];
 	get_csv("real_products.csv", (data) => {
-		console.log(data);
 		for (const line of data) {
-			possible_values.push(line.name);
+			possible_values.push(line);
 		}
+
+		add_suggestions_for("");
+		select_suggestion();
 	});
-	add_suggestions_for("");
 }
 
 function load() {
