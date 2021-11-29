5adf947c00acdc7f069d144fd08b320c88ea285c
Oscar Brunelle <oscar.brunelle@gmail.com>
2021-11-30 00:05:10 +0100

    Added current_date data_point

--- projects/tracker/graph/main.js
+++ projects/tracker/graph/main.js
@@ -1,23 +1,28 @@
 "use strict"
 
 function get_data_from_inventory() {
-	let cdate = new Date();
-	let cdate_string = `${cdate.getFullYear()}-${cdate.getMonth()+1}-${cdate.getDate()}`;
-
+	let current_date = new Date();
 	let data = [];
 	const inventory = get_inventory();
 	for (const item_key in inventory) {
 		let data_points = [];
+		let last_date, last_value;
 		for (const data_point of inventory[item_key]) {
 			data_points.push({
 				"date": data_point.date,
 				"value": data_point.quantity
 			});
+			if (last_date == null || data_point.date > last_date) {
+				last_date = data_point.date;
+				last_value = data_point.quantity;
+			}
 		}
-		data_points.push({
-			"date": cdate_string, // adding data_point to current date
-			"value": inventory[item_key][inventory[item_key].length-1].quantity // last quantity of item
-		});
+		if (last_value != null) {
+			data_points.push({
+				"date": current_date, // adding additional data_point to current date
+				"value": last_value // last quantity of item
+			});
+		}
 		data[item_key] = data_points;
 	}
 	return data;
