6662f2e155d6d1160c745f652f09812be5325221
Oscar Brunelle <oscar.brunelle@gmail.com>
2021-11-29 23:53:18 +0100

    Added actual data from the inventory

--- projects/tracker/graph/main.js
+++ projects/tracker/graph/main.js
@@ -1,16 +1,33 @@
 "use strict"
 
-function load_graph() {
+function get_data_from_inventory() {
+	let cdate = new Date();
+	let cdate_string = `${cdate.getFullYear()}-${cdate.getMonth()+1}-${cdate.getDate()}`;
+
 	let data = [];
 	const inventory = get_inventory();
-	console.log(inventory);
-	create_graph(document.querySelector("main"), {"months": [{
-		date: "2021-09-23T16:20:00",
-		value: "13",
-		label: "labeltest"
-	}, {
-		date: "2021-09-23T16:55:30",
-		value: "24",
-		label: "labeltest2"
-	}]});
+	for (const item_key in inventory) {
+		let data_points = [];
+		for (const data_point of inventory[item_key]) {
+			data_points.push({
+				"date": data_point.date,
+				"value": data_point.quantity
+			});
+		}
+		data_points.push({
+			"date": cdate_string, // adding data_point to current date
+			"value": inventory[item_key][inventory[item_key].length-1].quantity // last quantity of item
+		});
+		data[item_key] = data_points;
+	}
+	return data;
+}
+
+function load_graph() {
+	let data = get_data_from_inventory();
+	console.log(data);
+	create_graph(document.querySelector("main"), data, {
+		"min": -0.5,
+		"max": 2
+	});
 }
